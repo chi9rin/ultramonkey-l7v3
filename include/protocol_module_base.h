0252491cb7814ba01c5c692f8325219d3fe81654
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-16 06:01:36 +0000

    rs_listの扱いに付いて追加
    git-svn-id: http://10.144.169.20/repos/um/sdy/src/3.x-shamshel@5080 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/protocol_module_base.h
+++ include/protocol_module_base.h
@@ -46,12 +46,17 @@ public:
 
 //moduleがロードされた後で、VSからRS_List操作関数オブジェクトをもらう（it->begin(),it->end(),it->next()）
 //RS_Listは変更時にロックされるので、その時はbegin(),end(),next()しないような仕組みが必要
+	virtual boost::function< void ( void ) > rs_list_begin;
+	virtual boost::function< void ( void ) > rs_list_end;
+	virtual boost::function< void ( void ) > rs_list_next;
 
 //RS_List変更時、モジュールに対して変更の通知をする
 //変更開始と変更終了の通知をすれば、ロックタイミングをVS側からコントロールできる
+	virtual	handle_rs_list_edit_start();
+	virtual	handle_rs_list_edit_end();
 
-	virtual boost::function< void ( void ) > rs_list_lock;
-	virtual boost::function< void ( void ) > rs_list_unlock;
+	virtual	boost::function< void ( void ) > rs_list_lock;
+	virtual	boost::function< void ( void ) > rs_list_unlock;
 
 	virtual	void	set_parameter( const std::vector<std::string>& args ) = 0;
 	virtual	void	add_parameter( const std::vector<std::string>& args ) = 0;
@@ -59,6 +64,7 @@ public:
 	virtual	void	handle_sorry_enable() = 0;
 	virtual	void	handle_sorry_disable() = 0;
 
+	//use in upstream_thread
 	virtual	HANDLE_STATUS_TAG	handle_accept( const pthread_t up_thread_id, const pthread_t down_thread_id ) = 0;
 
 	virtual	HANDLE_STATUS_TAG	handle_client_recv( const pthread_t thread_id, const boost::array<char,n>& buffer, const int recv_len, boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
@@ -69,11 +75,13 @@ public:
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_connection_fail( const pthread_t thread_id ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_send( const pthread_t thread_id ) = 0;
 
+	//use in downstream_thread
 	virtual	HANDLE_STATUS_TAG	handle_realserver_recv( const pthread_t thread_id, const boost::asio::ip::basic_endpoint & rs_endpoint, const boost::array<char,n>& buffer, const int recv_len ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_recv( const pthread_t thread_id, const boost::array<char,n>& buffer, const int recv_len ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_clientrespondmessage_get( const pthread_t thread_id, boost::array<char,n>& buffer ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_client_send( const pthread_t thread_id, const boost::array<char,n>& buffer ) = 0;
 
+	//use in upstream/downstream thread
 	virtual	HANDLE_STATUS_TAG	handle_client_disconnect( const pthread_t thread_id ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_realserver_disconnect( const pthread_t thread_id, const boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_disconnect( const pthread_t thread_id ) = 0;
