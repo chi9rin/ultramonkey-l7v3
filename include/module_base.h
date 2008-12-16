338ad967c726b78d36a932bdfc8379f8ba2487f1
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-16 06:34:37 +0000

    header update
    git-svn-id: http://10.144.169.20/repos/um/sdy/src/3.x-shamshel@5082 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/module_base.h
+++ include/module_base.h
@@ -5,8 +5,10 @@
 class module_base
 {
 public:
+	module_base(){}
+	virtual ~module_base() = 0;
 	virtual	bool	is_tcp() = 0;
 	virtual	bool	is_udp() = 0;
-}
+};
 
 #endif //MODULE_BASE_H
--- include/protocol_module_base.h
+++ include/protocol_module_base.h
@@ -5,24 +5,12 @@
 #include <boost/function.hpp>
 #include <module_base.h>
 
-class protocol_module_base : public module_base
-{
-protected:
-
+class protocol_module_base : public module_base{
 public:
-	struct check_message_result
-	{
-		bool	flag;
-		string	message;
-	
-		bool	operator==( const check_message& in ){ return flag == in.flag; }
-		bool	operator!=( const check_message& in ){ return flag != in.flag; }
-	};
-
 	enum	HANDLE_STATUS_TAG
 	{
 		//use in upstream_thread
-		CLIENT_RECV,
+		CLIENT_RECV = 0,
 		CLIENT_RESPOND_SEND,
 		REALSERVER_CONNECT,
 		SORRYSERVER_CONNECT,
@@ -37,23 +25,30 @@ public:
 		CLIENT_DISCONNECT,
 		FINALIZE,
 		STOP
-	}
+	};
+	
 
-	protocol_module_base();
+	//this class is POD
+	struct check_message_result{
+		bool	flag;
+		string	message;
+		bool	operator==( const check_message& in ){ return flag == in.flag; }
+		bool	operator!=( const check_message& in ){ return flag != in.flag; }
+	};
 
+public:
+
+	// event function
+	protocol_module_base() = 0;
+	virtual ~protocol_module_base() = 0;
 	virtual	bool	is_use_sorry() = 0;
-	virtual	const check_message_result&	check_parameter( const std::vector<std::string> args ) =0;
+	virtual	const check_message_result&	check_parameter( const std::vector<std::string>& args ) =0;
 
-//moduleがロードされた後で、VSからRS_List操作関数オブジェクトをもらう（it->begin(),it->end(),it->next()）
-//RS_Listは変更時にロックされるので、その時はbegin(),end(),next()しないような仕組みが必要
 	virtual boost::function< void ( void ) > rs_list_begin;
 	virtual boost::function< void ( void ) > rs_list_end;
 	virtual boost::function< void ( void ) > rs_list_next;
 
-//RS_List変更時、モジュールに対して変更の通知をする
-//変更開始と変更終了の通知をすれば、ロックタイミングをVS側からコントロールできる
-	virtual	handle_rs_list_edit_start();
-	virtual	handle_rs_list_edit_end();
+	virtual	handle_rslist_update() = 0;
 
 	virtual	boost::function< void ( void ) > rs_list_lock;
 	virtual	boost::function< void ( void ) > rs_list_unlock;
@@ -65,25 +60,47 @@ public:
 	virtual	void	handle_sorry_disable() = 0;
 
 	//use in upstream_thread
-	virtual	HANDLE_STATUS_TAG	handle_accept( const pthread_t up_thread_id, const pthread_t down_thread_id ) = 0;
+	virtual	HANDLE_STATUS_TAG	handle_accept( const pthread_t upstream_thread_id,
+											   const pthread_t downstream_thread_id ) = 0;
 
-	virtual	HANDLE_STATUS_TAG	handle_client_recv( const pthread_t thread_id, const boost::array<char,n>& buffer, const int recv_len, boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_realserver_connect( const pthread_t thread_id, boost::array<char,n>& buffer ) = 0;
+	virtual	HANDLE_STATUS_TAG	handle_client_recv( const pthread_t thread_id,
+													const boost::array<char,MAX_BUFFER_SIZE>& recvbuffer,
+													const int recv_len,
+													boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
+
+	virtual	HANDLE_STATUS_TAG	handle_realserver_connect(	const pthread_t thread_id,
+															boost::array<char,MAX_BFFER_SIZE>& sendbuffer ) = 0;
+	
 	virtual	HANDLE_STATUS_TAG	handle_realserver_connection_fail( const pthread_t thread_id ) = 0;
+	
 	virtual	HANDLE_STATUS_TAG	handle_realserver_send( const pthread_t thread_id) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_sorryserver_connect( const pthread_t thread_id, boost::array<char,n>& buffer ) = 0;
+	
+	virtual	HANDLE_STATUS_TAG	handle_sorryserver_connect( const pthread_t thread_id,
+															boost::array<char,MAX_BUFFER_SIZE>& sendbuffer ) = 0;
+
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_connection_fail( const pthread_t thread_id ) = 0;
+	
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_send( const pthread_t thread_id ) = 0;
 
 	//use in downstream_thread
-	virtual	HANDLE_STATUS_TAG	handle_realserver_recv( const pthread_t thread_id, const boost::asio::ip::basic_endpoint & rs_endpoint, const boost::array<char,n>& buffer, const int recv_len ) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_sorryserver_recv( const pthread_t thread_id, const boost::array<char,n>& buffer, const int recv_len ) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_clientrespondmessage_get( const pthread_t thread_id, boost::array<char,n>& buffer ) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_client_send( const pthread_t thread_id, const boost::array<char,n>& buffer ) = 0;
+	virtual	HANDLE_STATUS_TAG	handle_realserver_recv( const pthread_t thread_id,
+														const boost::asio::ip::basic_endpoint & rs_endpoint,
+														const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
+														const int recv_len ) = 0;
+	
+	virtual	HANDLE_STATUS_TAG	handle_sorryserver_recv(	const pthread_t thread_id,
+															const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
+															const int recv_len ) = 0;
+	
+	virtual	HANDLE_STATUS_TAG	handle_clientrespondmessage_get(	const pthread_t thread_id,
+																	boost::array<char,MAX_BUFFER_SIZE>& sendbuffer ) = 0;
+	
+	virtual	HANDLE_STATUS_TAG	handle_client_send( const pthread_t thread_id ) = 0;
 
 	//use in upstream/downstream thread
 	virtual	HANDLE_STATUS_TAG	handle_client_disconnect( const pthread_t thread_id ) = 0;
-	virtual	HANDLE_STATUS_TAG	handle_realserver_disconnect( const pthread_t thread_id, const boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
+	virtual	HANDLE_STATUS_TAG	handle_realserver_disconnect(	const pthread_t thread_id,
+																const boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_sorryserver_disconnect( const pthread_t thread_id ) = 0;
 	virtual	HANDLE_STATUS_TAG	handle_finalize( const pthread_t thread_id ) = 0;
 
