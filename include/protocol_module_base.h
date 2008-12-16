4efe4bbc7af379c603ac086ce25abb97904e84af
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-16 12:06:30 +0000

    メソッドの引数等を確認、修正
    git-svn-id: http://10.144.169.20/repos/um/sdy/src/3.x-shamshel@5105 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/protocol_module_base.h
+++ include/protocol_module_base.h
@@ -42,7 +42,6 @@ public:
 		FINALIZE,
 		STOP
 	};
-	
 
 	//this class is POD
 	struct check_message_result{
@@ -52,7 +51,11 @@ public:
 		bool		operator!=( const check_message& in ){ return flag != in.flag; }
 	};
 protected:
-	boost::function< void ( const LOG_LEVEL_TAG, const LOG_CATEGORY_TAG, const std::string) >	logger;
+	boost::function< std::list<realserver>::iterator ( void ) > rs_list_begin;
+	boost::function< std::list<realserver>::iterator ( void ) > rs_list_end;
+	boost::function< std::list<realserver>::iterator ( void ) > rs_list_next;
+
+	boost::function< void ( const LOG_LEVEL_TAG, const std::string) >	logger;
 	boost::function< void ( const std::string&, unsigned int* ) > replication_pay_memory;
 public:
 
@@ -60,7 +63,7 @@ public:
 							boost::function< std::list<realserver>::iterator (void)> inlist_begin,
 							boost::function< std::list<realserver>::iterator (void)> inlist_end,
 							boost::function< std::list<realserver>::iterator (void)> inlist next,
-							boost::function< void ( const LOG_LEVEL_TAG, const LOG_CATEGORY_TAG, const std::string ) > inlog,
+							boost::function< void ( const LOG_LEVEL_TAG, const std::string ) > inlog,
 							boost::function< void ( std::string&, unsigned int* ) >  inreplication_pay_memory
 						) : rs_list_begin( inlist_begin ),
 							rs_list_end( inlist_end ),
@@ -74,11 +77,7 @@ public:
 	virtual	bool	is_use_sorry() = 0;
 	virtual	check_message_result&	check_parameter( const std::vector<std::string>& args ) const = 0;
 
-	virtual boost::function< std::list<realserver>::iterator ( void ) > rs_list_begin;
-	virtual boost::function< std::list<realserver>::iterator ( void ) > rs_list_end;
-	virtual boost::function< std::list<realserver>::iterator ( void ) > rs_list_next;
-
-	virtual	handle_rslist_update() = 0;
+	virtual	void	handle_rslist_update() = 0;
 
 	virtual	boost::function< void ( void ) > rs_list_lock;
 	virtual	boost::function< void ( void ) > rs_list_unlock;
@@ -89,19 +88,25 @@ public:
 	virtual	void	handle_sorry_enable() = 0;
 	virtual	void	handle_sorry_disable() = 0;
 
+	virtual	void	handle_session_initialize(
+									const boost::thread::id upthread_id,
+									const boost::thread::id down_thread_id ) = 0;
+	virtual	void	handle_session_finalize(
+									const boost::thread::id up_thread_id,
+									const boost::thread::id down_thread_id ) = 0;
+
 	//use in upstream_thread
 	virtual	EVENT_TAG	handle_accept(
-									const boost::thread::id upstream_thread_id,
-									const boost::thread::id downstream_thread_id ) = 0;
+									const boost::thread::id thread_id ) = 0;
 
 	virtual	EVENT_TAG	handle_client_recv(
 									const boost::thread::id thread_id,
 									const boost::array<char,MAX_BUFFER_SIZE>& recvbuffer,
-									const int recv_len,
-									const boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
+									const int recvlen ) = 0;
 
 	virtual	EVENT_TAG	handle_realserver_select(
 									const boost::thread::id thread_id,
+									boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
 
 	virtual	EVENT_TAG	handle_realserver_connect(
 									const boost::thread::id thread_id,
@@ -113,6 +118,10 @@ public:
 	virtual	EVENT_TAG	handle_realserver_send(
 									const boost::thread::id thread_id ) = 0;
 	
+	virtual	EVENT_TAG	handle_sorryserver_select(
+									const boost::thread::id thread_id,
+									boost::asio::ip::basic_endpoint & sorry_endpoint ) = 0;
+
 	virtual	EVENT_TAG	handle_sorryserver_connect(
 									const boost::thread::id thread_id,
 									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer ) = 0;
@@ -128,20 +137,18 @@ public:
 									const boost::thread::id thread_id,
 									const boost::asio::ip::basic_endpoint & rs_endpoint,
 									const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
-									const int recv_len ) = 0;
+									const int recvlen ) = 0;
 	
 	virtual	EVENT_TAG	handle_sorryserver_recv(
 									const boost::thread::id thread_id,
+									const boost::asio::ip::basic_endpoint & sorry_endpoint,
 									const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
-									const int recv_len ) = 0;
+									const int recvlen ) = 0;
 	
-	virtual	EVENT_TAG	handle_clientrespondmessage_get(
-									const boost::thread::id thread_id,
-									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer ) = 0;
-
 	virtual EVENT_TAG	handle_client_connection_check(
 									const boost::thread::id thread_id,
-									const boost::asio::ip::basic_endpoint& client_endpoint = 0);
+									const boost::asio::ip::basic_endpoint & recv_endpoint,
+									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer ) = 0;
 
 	virtual	EVENT_TAG	handle_client_send(
 									const boost::thread::id thread_id ) = 0;
@@ -155,10 +162,8 @@ public:
 									const boost::asio::ip::basic_endpoint & rs_endpoint ) = 0;
 	
 	virtual	EVENT_TAG	handle_sorryserver_disconnect(
-									const boost::thread::id thread_id ) = 0;
-	
-	virtual	EVENT_TAG	handle_session_finalize(
-									const boost::thread::id thread_id ) = 0;
+									const boost::thread::id thread_id,
+									const boost::asio::ip::basic_endpoint & sorry_endpoint ) = 0;
 
 //	virtual	EVENT_TAG	handle_event( const pthread_t thread_id, EVENT_TAG prev_status,  ) = 0;
 };
