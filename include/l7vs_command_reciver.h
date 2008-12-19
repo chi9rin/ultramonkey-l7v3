491910ce10b5779d0d85738fe8d03f753f841058
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-19 06:40:53 +0000

    単語の誤字修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5283 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/l7vs_command_reciver.h
+++ include/l7vs_command_reciver.h
@@ -5,9 +5,9 @@
 
 namespace l7vsd{
 
-class	command_reciver{
+class	command_receiver{
 protected:
-	command_reciver(){}
+	command_receiver(){}
 	// l7vsd_mainthread										main_thread;
 	boost::asio::io_service&								dispatcher;
 	boost::asio::local::stream_protocol::acceptor		acceptor_;
@@ -15,8 +15,8 @@ protected:
 public:
 	command_reciver(	boost::asio::io_service& io_service, const std::string& file ) 
 		:	dispatcher( io_service ),
-			acceprot_( io_service, boost::asio::local::stream_protocol::endpoint( file ) );
-	~command_reciver();
+			acceptor_( io_service, boost::asio::local::stream_protocol::endpoint( file ) );
+	~command_receiver();
 };
 
 };	//namespace l7vsd
