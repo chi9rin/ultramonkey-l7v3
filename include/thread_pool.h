fd91ad4e3b060a761ef8880d946cd28de0e11f73
takamaru <takamaru@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-26 02:05:30 +0000

    thread_pool更新
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5535 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/thread_pool.h
+++ include/thread_pool.h
@@ -4,22 +4,43 @@
 #include <boost/thread.hpp>
 
 class thread_pool {
+protected:
+	typedef std::map<boost::thread::id, tcp_session*> session_map_type;
+
 public:
 	template <typename F>
-	void schedule(F threadfunc) {
-		tg_->create_thread(threadfunc);
+	void schedule(tcp_session& sess) {
+		sess.initialize();
+
+		tg_->create_thread(sess.down_thread_run());
+		tg_->create_thread(sess.up_thread_run());
+
+		session_map.add(sess.get_down_thread_id(), &sess);
+		
 	}
 
-	void wait() {
+	//virtual_serviceが終了するときにsessionをすべて終了させる関数
+	void stop_threads() {
+		for (session_map::iterator itr = session_map.begin(); itr != session_map.end(); ++itr) {
+			itr->second->finalize();
+		}
 		tg_->join_all();
 	}
 
-	thread_pool() :
+	//sessionが終了するときに呼ぶ関数
+	void end_session(boost::thread::id id) {
+		vs.end_session(session_map[id]->second);
+	}
+
+	thread_pool(virtual_service& vs) :
+		vs_(vs),
 		tg_(new boost::thread_group) {}
 
 
 protected:
+	virtual_service& vs_;
 	boost::shared_ptr<boost::thread_group> tg_;
+	session_map_type session_map;
 };
 
 #endif	//__THREAD_POOL_H__
