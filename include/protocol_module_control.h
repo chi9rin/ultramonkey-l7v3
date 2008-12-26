dc5b2858123f7ea8cc4aaf02e8980b3acea5e394
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2008-12-26 06:26:35 +0000

    l7vs_realserver.hをrealserver.hに変更
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5559 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/protocol_module_control.h
+++ include/protocol_module_control.h
@@ -19,29 +19,47 @@
 
 namespace l7vs{
 
+// protocol module control class is load protocol module from shared object file.
+// many virtual service class used protocol module instance.
+// but, shared object load is once. and unload when vitual service refarence count is zero.
 class	protocol_module_control : public module_control_base : public boost::noncopyable{
 public:
-	typedef	std::map<std::string,protocol_module_info>
-								name_module_info_map;
+
+	// logger function object typedef
 	typedef	boost::function< void ( const LOG_LEVEL_TAG, const std::string ) >
 								logger_func_type;
-
+	// module create function object typedef
+	typedef	boost::function< protocol_module_base* ( logger_func_type ) >
+								create_func_type;
+	// module destroy function object typedef
+	typedef	boost::function< void ( protocol_module_base* ) >
+								destroy_func;
+	
+	// protocol module information structure.
 	struct	protocol_module_info{
-		unsigned int	ref_count;
-		boost::function<protocol_module_base*(logger_func_type)>
-						create_func;
-		boost::function<void(protocol_module_base*)>
-						destroy_func;
-		protocol_module_info() : ref_count(0) {}
+		unsigned int		ref_count;			// refarence count
+		create_func_type	create_func;		// create function object
+		destroy_func		destroy_func;		// destroy function object
+		protocol_module_info() : ref_count(0) {}	// constractor
 	};
 
+	// name module map type typedef
+	typedef	std::map<std::string,protocol_module_info>
+								name_module_info_map;
+
+
 protected:
+	// load module information hash map,
 	name_module_info_map	loadmodule_map;
+	// this mutex used loadmodule_map read/write/change.
 	boost::mutex			loadmodule_map_mutex;
 public:
+	// instance getter function.
 	static protocol_module_control&	getInstance();
+	// load module function
 	protocol_module_base*	load_module(	const	std::string& modulename,
 											logger_func_type inlog );
+	// unload module function.
 	void					unload_module(	const std::string& modulename,
 											protocol_module_base* module_ptr );
 }
