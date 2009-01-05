a3f4ed942cd476e9056462201aa593eab59b7ab6
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-05 10:37:23 +0000

    doxgenタグ追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5635 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/protocol_module_control.h
+++ include/protocol_module_control.h
@@ -1,6 +1,6 @@
 //
-//	@file	protocol_module_base.h
-//	@brief	shared object protocol module absctract class
+//	@file	protocol_module_control.h
+//	@brief	control load/unload shared object protocol module
 //
 //	copyright (c) sdy corporation. 2008
 //	mail: h dot okada at sdy dot co dot jp
@@ -26,17 +26,17 @@ namespace l7vs{
 // but, shared object load is once. and unload when vitual service refarence count is zero.
 class	protocol_module_control : public module_control_base, private boost::noncopyable{
 public:
-	// logger function object typedef
+	//! logger function object typedef
 	typedef	boost::function< void ( const LOG_LEVEL_TAG, const std::string ) >
 								logger_func_type;
-	// module create function object typedef
+	//! module create function object typedef
 	typedef	boost::function< protocol_module_base* ( logger_func_type ) >
 								create_func_type;
-	// module destroy function object typedef
+	//! module destroy function object typedef
 	typedef	boost::function< void ( protocol_module_base* ) >
 								destroy_func_type;
 	
-	// protocol module information structure.
+	//! protocol module information structure.
 	struct	protocol_module_info{
 		unsigned int		ref_count;			// refarence count
 		create_func_type	create_func;		// create function object
@@ -44,27 +44,27 @@ public:
 		protocol_module_info() : ref_count(0) {}	// constractor
 	};
 
-	// name module map type typedef
+	//! name module map type typedef
 	typedef	std::map<std::string,protocol_module_info>
 								name_module_info_map;
 
 protected:
-	// load module information hash map,
+	//! load module information hash map,
 	name_module_info_map	loadmodule_map;
-	// this mutex used loadmodule_map read/write/change.
+	//! this mutex used loadmodule_map read/write/change.
 	boost::mutex			loadmodule_map_mutex;
 
 public:
-	// instance getter function.
+	//! instance getter function.
 	static protocol_module_control&	getInstance();
-	// initialize
+	//! initialize
 	void	initialize( const std::string& infile_path );
-	// finalize
+	//! finalize
 	void	finalize();
-	// load module function
+	//! load module function
 	protocol_module_base*	load_module(	const	std::string& modulename,
 											logger_func_type inlog );
-	// unload module function.
+	//! unload module function.
 	void					unload_module(	const std::string& modulename,
 											protocol_module_base* module_ptr );
 };
--- src/protocol_module_control.cpp
+++ src/protocol_module_control.cpp
@@ -1,27 +1,69 @@
+//
+//	@file	protocol_module_control.cpp
+//	@brief	control load/unload shared object protocol module
+//
+//	copyright (c) sdy corporation. 2008
+//	mail: h dot okada at sdy dot co dot jp
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
 #include	"protocol_module_control.h"
 
 namespace l7vs{
 
+/*!
+ * returns singleton instance.
+ *
+ * @param   void
+ * @return  instance
+ */
 protocol_module_control&
 protocol_module_control::getInstance(){
 	static	protocol_module_control	instance;
 	return	instance;
 }
 
+/*!
+ * initialize class.
+ *
+ * @param   filepath
+ * @return  void
+ */
 void
 protocol_module_control::initialize( const std::string& infile_path ){
 }
 
+/*!
+ * finalize class.
+ *
+ * @param   void
+ * @return  void
+ */
 void
 protocol_module_control::finalize(){
 }
 
+/*!
+ * load shared object.
+ *
+ * @param   modulename
+ * @param   loggingfunction
+ * @return  pointer for protocol_module_base class instance
+ */
 protocol_module_base*
 protocol_module_control::load_module(	const	std::string& modulename,
 											logger_func_type inlog ){
 
 }
 
+/*!
+ * unload shared object.
+ *
+ * @param   modulename
+ * @param   pointer for protocol_module_base class instance
+ * @return  void
+ */
 void
 protocol_module_control::unload_module(	const std::string& modulename,
 											protocol_module_base* module_ptr ){
