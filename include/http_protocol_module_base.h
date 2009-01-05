39d4a581b4fc7bd4eb83305cf7945600743b9a87
yatabe <yatabe@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-05 02:04:52 +0000

    http_protocol_module_base.h修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5602 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/http_protocol_module_base.h
+++ include/http_protocol_module_base.h
@@ -70,97 +70,98 @@ public:
 	check_message_result&	set_parameter( const std::vector<std::string>& args );
 	check_message_result&	add_parameter( const std::vector<std::string>& args );
 
-	void	register_schedule( schedule inschedule );
+	void	register_schedule( tcp_schedule_func_type inschedule );
 
 	void	register_replication_area_lock(
-								boost::function< void( void ) > inlock_func );
+									boost::function< void( void ) > inlock_func );
 
 	void	register_replication_area_unlock(
-								boost::function< void( void ) > inunlock_func );
+									boost::function< void( void ) > inunlock_func );
 
 	//use in upstream_thread
 	EVENT_TAG	handle_session_initialize(
-								const boost::thread::id upthread_id,
-								const boost::thread::id down_thread_id );
+									const boost::thread::id upthread_id,
+									const boost::thread::id down_thread_id );
 	EVENT_TAG	handle_session_finalize(
-								const boost::thread::id up_thread_id,
-								const boost::thread::id down_thread_id );
+									const boost::thread::id up_thread_id,
+									const boost::thread::id down_thread_id );
 	EVENT_TAG	handle_accept(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 
 	EVENT_TAG	handle_client_recv(
-								const boost::thread::id thread_id,
-								const boost::array<char,MAX_BUFFER_SIZE>& recvbuffer,
-								const int recvlen );
+									const boost::thread::id thread_id,
+									const boost::array<char,MAX_BUFFER_SIZE>& recvbuffer,
+									const int recvlen );
 
 	EVENT_TAG	handle_realserver_select(
-								const boost::thread::id thread_id,
-								boost::asio::ip::basic_endpoint & rs_endpoint );
+									const boost::thread::id thread_id,
+									boost::asio::ip::tcp::endpoint& rs_endpoint );
 
 	EVENT_TAG	handle_realserver_connect(
-								const boost::thread::id thread_id,
-								const boost::array<char,MAX_BFFER_SIZE>& sendbuffer );
+									const boost::thread::id thread_id,
+									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer );
 	
 	EVENT_TAG	handle_realserver_connection_fail(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint );
 	
 	EVENT_TAG	handle_realserver_send(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 	
 	EVENT_TAG	handle_sorryserver_select(
-								const boost::thread::id thread_id,
-								boost::asio::ip::basic_endpoint & sorry_endpoint );
+									const boost::thread::id thread_id,
+									boost::asio::ip::tcp::endpoint & sorry_endpoint );
 
 	EVENT_TAG	handle_sorryserver_connect(
-								const boost::thread::id thread_id,
-								const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer );
+									const boost::thread::id thread_id,
+									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer );
 
 	EVENT_TAG	handle_sorryserver_connection_fail(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 	
 	EVENT_TAG	handle_sorryserver_send(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 
 	//use in downstream_thread
 	EVENT_TAG	handle_realserver_recv(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint & rs_endpoint,
-								const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
-								const int recvlen );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint & rs_endpoint,
+									const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
+									const int recvlen );
 	
 	EVENT_TAG	handle_sorryserver_recv(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint & sorry_endpoint,
-								const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
-								const int recvlen );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint & sorry_endpoint,
+									const boost::array<char,MAX_BUFFER_SIZE>& readbuffer,
+									const int recvlen );
 	
 	EVENT_TAG	handle_response_send_inform(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 
 	EVENT_TAG	handle_client_connection_check(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint & recv_endpoint,
-								const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint & recv_endpoint,
+									const boost::array<char,MAX_BUFFER_SIZE>& sendbuffer );
 
 	EVENT_TAG	handle_client_send(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 
 	//use in upstream/downstream thread
 	EVENT_TAG	handle_client_disconnect(
-								const boost::thread::id thread_id );
+									const boost::thread::id thread_id );
 	
 	EVENT_TAG	handle_sorry_enable( const boost::thread::id upthread_id );
 
 	EVENT_TAG	handle_sorry_disable( const boost::thread::id upthread_id );
 
 	EVENT_TAG	handle_realserver_disconnect(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint & rs_endpoint );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint & rs_endpoint );
 	
 	EVENT_TAG	handle_sorryserver_disconnect(
-								const boost::thread::id thread_id,
-								const boost::asio::ip::basic_endpoint & sorry_endpoint );
+									const boost::thread::id thread_id,
+									const boost::asio::ip::tcp::endpoint & sorry_endpoint );
+
 
 };
 
