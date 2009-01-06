fadc03dbda08b076e5bfc3544546d23ac2d8e4f1
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-06 05:43:12 +0000

    protocol_module_controlのunittestを追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5660 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ unit_tests/module_controls/protocol_module_control_test.cpp
@@ -0,0 +1,27 @@
+#define	MAX_BUFFER_SIZE	(65535)
+
+
+#include <boost/test/included/unit_test.hpp>
+#include "../../src/protocol_module_control.cpp"
+
+using namespace boost::unit_test;
+
+//test case1.
+void	protocol_module_control_test(){
+	l7vs::protocol_module_control& control = l7vs::protocol_module_control::getInstance();
+	control.initialize( "./" );
+	control.finalize();
+	control.initialize( NULL );
+
+}
+
+test_suite*	init_unit_test_suite( int argc, char* argv[] ){
+
+	// create unit test suite
+	test_suite* ts = BOOST_TEST_SUITE( "protocol_module_control" );
+	ts->add( BOOST_TEST_CASE( &protocol_module_control_test ) );
+
+	framework::master_test_suite().add( ts );
+
+	return 0;
+}
