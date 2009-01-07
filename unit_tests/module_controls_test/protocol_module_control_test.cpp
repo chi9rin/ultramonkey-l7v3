8aaa721a78996d8a6bb19b36c1621cc6e6e4714f
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-07 09:00:23 +0000

    テスト項目追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5714 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/module_controls_test/protocol_module_control_test.cpp
+++ unit_tests/module_controls_test/protocol_module_control_test.cpp
@@ -13,21 +13,45 @@ using namespace boost::unit_test;
 
 //test case1.
 void	protocol_module_control_test(){
-	l7vs::protocol_module_base*		protomod = NULL;
+	//============================================
+	//protocol_module_control
+	//1
+	//getInstanceメソッドのテスト
+	//getInstanceによって取得したcontrolとcontrol_2のインスタンスが同一であることを確認する。 
+	//============================================
 	l7vs::protocol_module_control& control = l7vs::protocol_module_control::getInstance();
-	//call load_module before initialize
-	try{
-		protomod = control.load_module( PM1 );
-	}
-//	catch( l7vs::module_control_error& err ){
-//		std::cout << err.message << std::endl;
-//	}
-	catch(...){
-	}
+	l7vs::protocol_module_control& control_2 = l7vs::protocol_module_control::getInstance();
+
+	BOOST_CHECK_EQUAL( &control, &control_2 );
 
 	//test initialize and finalize
 	control.initialize( "./" );
 	control.finalize();
+
+
+	//============================================
+	//protocol_module_control
+	//2
+	//load_moduleメソッドのテスト(正常系その１)
+	//ProtocolModuleクラスのインスタンスが取得できること
+	//例外が発生しないこと
+	//指定したモジュール名と取得したモジュール名が同じこと 
+	//============================================
+	control.initialize( "./" );
+	l7vs::protocol_module_base*		protomod = NULL;
+	try{
+		protomod = control.load_module( PM1 );
+	}
+	catch(...){
+		BOOST_ERROR( "exception : load_module" );
+	}
+	BOOST_CHECK( NULL != protomod );
+//	BOOST_CHECK_EQUAL( PM1, protomod.get_name() );
+
+}
+
+//test case2
+void	protocol_module_control_test_thread(){
 
 }
 
@@ -35,7 +59,10 @@ test_suite*	init_unit_test_suite( int argc, char* argv[] ){
 
 	// create unit test suite
 	test_suite* ts = BOOST_TEST_SUITE( "protocol_module_control" );
+
+	// add test case to test suite
 	ts->add( BOOST_TEST_CASE( &protocol_module_control_test ) );
+	ts->add( BOOST_TEST_CASE( &protocol_module_control_test_thread ) );
 
 	framework::master_test_suite().add( ts );
 
