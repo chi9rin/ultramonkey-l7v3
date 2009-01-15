4e700c4225a55a8327276a7a0fb81149c49dfbfc
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-15 08:32:43 +0000

    http_protocol_module_base修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@5949 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- module/protocol/http_protocol_module_base.h
+++ module/protocol/http_protocol_module_base.h
@@ -14,42 +14,50 @@
 namespace l7vs{
 
 class http_protocol_module_base : public protocol_module_base {
+public:
+	typedef	boost::array<char, MAX_BUFFER_SIZE>	contenor_type;
 protected:
-
-	// http function
-	int		check_http_method(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len );
-	int		check_http_version(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len );
-	int		check_status_code(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len );
-	bool	find_uri(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len,
-								int& uri_offset,
-								int& uri_len );
-	bool	find_status_code(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len,
-								int& status_code_offset,
-								int& status_code_len );
-	bool	find_http_header(
-								const boost::array<char,MAX_BUFFER_SIZE>& buffer,
-								const int buffer_len,
-								const boost::array<char,MAX_BUFFER_SIZE>& http_header_name,
-								const int http_header_name_len,
-								int& http_header_offset,
-								int& http_header_len );
+	//! check http method function
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @return bool				http_protocol is true, no http_protocol is false 
+	bool	check_http_method(	const contenor_type&, const int ) const;
+	//! check http version function
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @return	double 				http version 1.0 or 1.1
+	double	check_http_version(	const contenor_type&, const int ) const;
+	//! ch1eck http status code function
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @return	bool				status code is nomal or error
+	bool		check_status_code(	const contenor_type&, const int );
+	//! check serch uri function
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @param int& uri length
+	//! @return contenor_type::iterator	url head pos. not find url is contenor_type::end()
+	contenor_type::iterator	find_uri( const contenor_type&, const int, int&);
+	//! check serch status function
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @param int& status code length
+	//! @return contenor_type::iterator status pos. not find status is contenor_type::end()
+	contenor_type::iterator find_status_code(	const contenor_type&, const int, int& );
+	//! serch http header
+	//! @param const contenor_type	buffer
+	//! @param const int			length
+	//! @param const string&		header name
+	//! @param int&  int			header_length
+	//! @return contenor_type::iterator http header pos. not find status is contenor_type::end()
+	contenor_type::iterator find_http_header(	const contenor_type&, const int, const string&, int& );
 
 public:
-
+	//! constractor
+	//! @param logger_func_type		logger_function object
 	http_protocol_module_base( logger_func_type inlog ) : logger( inlog );
-
+	//! destractor
 	virtual	~http_protocol_module_base() = 0;
-
 };
 
 } // namespace l7vsd
