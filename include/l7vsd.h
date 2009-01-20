0dddbbe9d9e34f1e947e88270d2e4a554efd7fe8
takamaru <takamaru@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-20 05:44:34 +0000

    l7vsd.h修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6113 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/l7vsd.h
+++ include/l7vsd.h
@@ -16,11 +16,11 @@ namespace l7vs{
 //! l7vsd main class
 class l7vsd{
 public:
-	typedef shared_ptr< command_receiver >	command_receiver_ptr;	//! shared_ptr command_receiver typedef
-	typedef shared_ptr< replication >		replication_ptr;		//! shared_ptr replication typedef
-	typedef shared_ptr< snmp_bridge >		snmp_bridge_ptr;		//! shared_ptr snmp_bridge typedef
+	typedef shared_ptr< command_receiver >	command_receiver_ptr;	//!< shared_ptr command_receiver typedef
+	typedef shared_ptr< replication >		replication_ptr;		//!< shared_ptr replication typedef
+	typedef shared_ptr< snmp_bridge >		snmp_bridge_ptr;		//!< shared_ptr snmp_bridge typedef
 
-	typedef std::list< virtual_service >	vs_list_type;			//! virtual_service list typedef
+	typedef std::list< virtual_service >	vs_list_type;			//!< virtual_service list typedef
 	
 	struct l7vsd_operation_result{
 		bool	flag;
@@ -33,51 +33,51 @@ public:
 	};
 
 protected:
-	boost::thread_group			vs_threads;			//! virtual_service thread group
-	boost::asio::io_service		dispatcher;			//! dispatcher
+	boost::thread_group			vs_threads;			//!< virtual_service thread group
+	boost::asio::io_service		dispatcher;			//!< dispatcher
 
-	vs_list_type				vs_list;			//! virtual_service list
+	vs_list_type				vs_list;			//!< virtual_service list
 
-	command_receiver_ptr		cmd_receiver;		//! command_receiver ptr
-	replication_ptr				repli;				//! replication ptr
-	snmp_bridge_ptr				bridge;				//! snmp_bridge ptr
+	command_receiver_ptr		cmd_receiver;		//!< command_receiver ptr
+	replication_ptr				repli;				//!< replication ptr
+	snmp_bridge_ptr				bridge;				//!< snmp_bridge ptr
 
-	vs_list_type::iterator		search_vslist( virtualservice_element& );	//! vs_list search function
+	vs_list_type::iterator		search_vslist( virtualservice_element& );	//!< vs_list search function
 
 public:
-	l7vsd_operation_result		list_virtual_service( std::vector<virtualservice_element>&  );	//! virtual_service list command
+	l7vsd_operation_result		list_virtual_service( std::vector<virtualservice_element>&  );	//!< virtual_service list command
 
-	l7vsd_operation_result		add_virtual_service( const virtualservice_element& );	//! virtual_service add command
-	l7vsd_operation_result		del_virtual_service( const virtualservice_element& );	//! virtual_service del command
-	l7vsd_operation_result		edit_virtual_service( const virtualservice_element& );	//! virtual_service edit command
+	l7vsd_operation_result		add_virtual_service( const virtualservice_element& );	//!< virtual_service add command
+	l7vsd_operation_result		del_virtual_service( const virtualservice_element& );	//!< virtual_service del command
+	l7vsd_operation_result		edit_virtual_service( const virtualservice_element& );	//!< virtual_service edit command
 
-	l7vsd_operation_result		add_real_server( const virtualservice_element& );	//! real_server add command
-	l7vsd_operation_result		del_real_server( const virtualservice_element& );	//! real_server del command
-	l7vsd_operation_result		edit_real_server( const virtualservice_element& );	//! real_server edit command
+	l7vsd_operation_result		add_real_server( const virtualservice_element& );	//!< real_server add command
+	l7vsd_operation_result		del_real_server( const virtualservice_element& );	//!< real_server del command
+	l7vsd_operation_result		edit_real_server( const virtualservice_element& );	//!< real_server edit command
 
-	l7vsd_operation_result		flush_virtual_service();	//! all virtual_service delete command
+	l7vsd_operation_result		flush_virtual_service();	//!< all virtual_service delete command
 
-	l7vsd_operation_result		get_replication_info( REPLICATION_MODE_TAG& );	//! get replication info command
-	l7vsd_operation_result		start_replication();		//! start replication command
-	l7vsd_operation_result		stop_replication();			//! stop replication command
-	l7vsd_operation_result		dump_replication_memory();	//! dump replication memory command
-	l7vsd_operation_result		force_replicate();			//! force replicate command
+	l7vsd_operation_result		get_replication_info( REPLICATION_MODE_TAG& );	//!< get replication info command
+	l7vsd_operation_result		start_replication();		//!< start replication command
+	l7vsd_operation_result		stop_replication();			//!< stop replication command
+	l7vsd_operation_result		dump_replication_memory();	//!< dump replication memory command
+	l7vsd_operation_result		force_replicate();			//!< force replicate command
 
-	l7vsd_operation_result		get_log_level( const LOG_CATEGORY_TAG, LOG_LEVEL_TAG& );		//! get loglevel command
-	l7vsd_operation_result		set_log_level( const LOG_CATEGORY_TAG, const LOG_LEVEL_TAG );	//! set loglevel command
-	l7vsd_operation_result		set_log_level_all( const LOG_LEVEL_TAG );						//! set all category loglevel command
+	l7vsd_operation_result		get_log_level( const LOG_CATEGORY_TAG, LOG_LEVEL_TAG& );		//!< get loglevel command
+	l7vsd_operation_result		set_log_level( const LOG_CATEGORY_TAG, const LOG_LEVEL_TAG );	//!< set loglevel command
+	l7vsd_operation_result		set_log_level_all( const LOG_LEVEL_TAG );						//!< set all category loglevel command
 
-	l7vsd_operation_result		get_snmp_connect_status( int& );									//! get snmp connect status command
-	l7vsd_operation_result		get_snmp_log_level( const LOG_CATEGORY_TAG, LOG_LEVEL_TAG& );		//! get snmp loglevel command
-	l7vsd_operation_result		set_snmp_log_level( const LOG_CATEGORY_TAG, const LOG_LEVEL_TAG );	//! set snmp loglevel command
-	l7vsd_operation_result		set_snmp_log_level_all( const LOG_LEVEL_TAG );						//! set snmp all loglevel command
+	l7vsd_operation_result		get_snmp_connect_status( int& );									//!< get snmp connect status command
+	l7vsd_operation_result		get_snmp_log_level( const LOG_CATEGORY_TAG, LOG_LEVEL_TAG& );		//!< get snmp loglevel command
+	l7vsd_operation_result		set_snmp_log_level( const LOG_CATEGORY_TAG, const LOG_LEVEL_TAG );	//!< set snmp loglevel command
+	l7vsd_operation_result		set_snmp_log_level_all( const LOG_LEVEL_TAG );						//!< set snmp all loglevel command
 
-	l7vsd_operation_result		reload_parameter( const PARAMETER_COMPONENT_TAG );	//! reload component parameter command
+	l7vsd_operation_result		reload_parameter( const PARAMETER_COMPONENT_TAG );	//!< reload component parameter command
 
-	void						run();		//! l7vsd run method
+	void						run();		//!< l7vsd run method
 
 protected:
-	bool						is_exit_requested();		//! check if exit requested
+	bool						is_exit_requested();		//!< check if exit requested
 
 };
 
