d1a53dbaa1345d77e3c1b8347422d2e1489437c4
fujita <fujita@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-21 09:20:36 +0000

    realserver追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6200 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ src/realserver.cpp
@@ -0,0 +1,45 @@
+//
+//	@file	l7vs_realserver.cpp
+//	@brief	realserver data class
+//
+//	copyright (c) xxx corporation. 2009
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+#include	"realserver.h"
+
+namespace l7vs{
+
+void	realserver::increment_active(){
+	boost::mutex::scoped_lock( active_mutex );
+
+	nactive++;
+}
+
+void	realserver::decrement_active(){
+	boost::mutex::scoped_lock( active_mutex );
+
+	nactive--;
+}
+
+void	realserver::increment_inact(){
+	boost::mutex::scoped_lock( inact_mutex );
+
+	ninact++;
+}
+
+int		realserver::get_active(){
+	boost::mutex::scoped_lock( active_mutex );
+
+	return nactive;
+}
+
+int		realserver::get_inact(){
+	boost::mutex::scoped_lock( inact_mutex );
+
+	return ninact;
+}
+
+}	//namespace l7vs
