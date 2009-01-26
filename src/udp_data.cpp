1f57eb791a1c7805d453ac56ae88d89132fb4888
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-26 11:39:34 +0000

    udp_data class unit test complete
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6334 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/udp_data.cpp
+++ src/udp_data.cpp
@@ -1,25 +1,38 @@
+//
+//!	@file	udp_data.cpp
+//!	@brief	udp request processing data class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #include "udp_data.h"
 
 namespace l7vs{
-
+	//! construcor
 	udp_data::udp_data(){
-		initialize();
-	}
-
-	udp_data::~udp_data(){
-	}
-
-	void udp_data::initialize(){
 		endpoint_info = boost::asio::ip::udp::endpoint();
 	}
-
+	//! destructor
+	udp_data::~udp_data(){
+	}
+	//! initialize
+	void udp_data::initialize(){
+		endpoint_info = boost::asio::ip::udp::endpoint();
+		data_buff_base::initialize();
+	}
+	//! get endpoint
+	boost::asio::ip::udp::endpoint udp_data::get_endpoint(){
+		return endpoint_info;
+	}
+	//! set endpoint
 	void udp_data::set_endpoint(const boost::asio::ip::udp::endpoint set_endpoint){
 		endpoint_info = set_endpoint;
 	}
 
-	boost::asio::ip::udp::endpoint udp_data::get_endpoint(){
-		return endpoint_info;
-	}
 
 }// namespace l7vsd
 
