4eb2c07edb297de5d4e3ef875fa07ae055192a39
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-26 11:43:13 +0000

    tcp_data class unit test complete
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6336 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/tcp_data.h
+++ include/tcp_data.h
@@ -1,21 +1,40 @@
+//
+//!	@file	tcp_data.h
+//!	@brief	tcp session processing data class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef TCP_DATA_H
 #define TCP_DATA_H
-
 
 #include "data_buff_base.h"
 
 namespace l7vs{
 
+//!	@class	tcp_data
+//! @brief	this class is tcp session object use data for processing.
 	class tcp_data : public data_buff_base{
-		protected:
-			boost::asio::ip::tcp::endpoint endpoint_info;
 		public:
+			//! construcor
 			tcp_data();
+			//! destructor
 			~tcp_data();
+			//! initialize
 			void initialize();
-			void set_endpoint(const boost::asio::ip::tcp::endpoint set_endpoint);
+			//! get endpoint
+			//! @return		endpoint_info
 			boost::asio::ip::tcp::endpoint get_endpoint();
-
+			//! set endpoint
+			//! @param[in]	copy endpoint
+			void set_endpoint(const boost::asio::ip::tcp::endpoint set_endpoint);
+		protected:
+			//! tcp endpoint
+			boost::asio::ip::tcp::endpoint endpoint_info;
 	};// class tcp_data
 }// namespace l7vs
 
