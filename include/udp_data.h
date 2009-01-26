ef99eaa043ec3c187b57c2155e007d4bea663505
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-26 11:43:49 +0000

    udp data class unit test complete
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6337 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/udp_data.h
+++ include/udp_data.h
@@ -1,3 +1,14 @@
+//
+//!	@file	udp_data.h
+//!	@brief	udp request processing data class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef UDP_DATA_H
 #define UDP_DATA_H
 
@@ -6,15 +17,25 @@
 
 namespace l7vs{
 
+//!	@class	udp_data
+//! @brief	this class is udp session object use data for processing.
 	class udp_data : public data_buff_base{
-		protected:
-			boost::asio::ip::udp::endpoint endpoint_info;
 		public:
+			//! construcor
 			udp_data();
+			//! destructor
 			~udp_data();
+			//! initialize
 			void initialize();
-			void set_endpoint(const boost::asio::ip::udp::endpoint set_endpoint);
+			//! get endpoint
+			//! @return		endpoint_info
 			boost::asio::ip::udp::endpoint get_endpoint();
+			//! set endpoint
+			//! @param[in]	copy endpoint
+			void set_endpoint(const boost::asio::ip::udp::endpoint set_endpoint);
+		protected:
+			//! udp endpoint
+			boost::asio::ip::udp::endpoint endpoint_info;
 
 	};// class udp_data
 }// namespace l7vs
