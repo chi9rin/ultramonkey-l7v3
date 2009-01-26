040c46695f7631437f14be6d02415d99b7b9c4b0
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-26 11:42:04 +0000

    tcp_data class unit test complete
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6335 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/tcp_data.cpp
+++ src/tcp_data.cpp
@@ -1,25 +1,40 @@
+//
+//!	@file	tcp_data.cpp
+//!	@brief	tcp session processing data class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #include "tcp_data.h"
 
 namespace l7vs{
-
+	//! construcor
 	tcp_data::tcp_data(){
-		initialize();
-	}
-
-	tcp_data::~tcp_data(){
-	}
-
-	void tcp_data::initialize(){
 		endpoint_info = boost::asio::ip::tcp::endpoint();
 	}
-
+	//! destructor
+	tcp_data::~tcp_data(){
+	}
+	//! initialize
+	void tcp_data::initialize(){
+		endpoint_info = boost::asio::ip::tcp::endpoint();
+		data_buff_base::initialize();
+	}
+	//! get endpoint
+	//! @return		endpoint_info
+	boost::asio::ip::tcp::endpoint tcp_data::get_endpoint(){
+		return endpoint_info;
+	}
+	//! set endpoint
+	//! @param[in]	copy endpoint
 	void tcp_data::set_endpoint(const boost::asio::ip::tcp::endpoint set_endpoint){
 		endpoint_info = set_endpoint;
 	}
 
-	boost::asio::ip::tcp::endpoint tcp_data::get_endpoint(){
-		return endpoint_info;
-	}
 
 }// namespace l7vsd
 
