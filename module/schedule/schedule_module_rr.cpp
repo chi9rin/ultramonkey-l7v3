4b4027e717c9f0c01420c9e2cd24d95560ee2652
fujita <fujita@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 02:49:41 +0000

    schedule_module_rr及びテスト環境
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6358 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ module/schedule/schedule_module_rr.cpp
@@ -0,0 +1,183 @@
+//
+//	@file	schedule_module_rr.cpp
+//	@brief	shared object schedule module class
+//
+//	copyright (c) xxx corporation. 2009
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+#include "schedule_module_rr.h"
+
+namespace l7vs{
+
+//!	constractor
+schedule_module_round_robin::schedule_module_round_robin() : schedule_module_base( "schedule round robin" ){
+}
+
+//! destractor
+schedule_module_round_robin::~schedule_module_round_robin(){}
+
+//!	initialize function
+void	schedule_module_round_robin::initialize(){
+	boost::asio::ip::tcp::endpoint	tcp_local_endpoint ;
+	boost::asio::ip::udp::endpoint	udp_local_endpoint ;
+
+	tcp_endpoint = tcp_local_endpoint ;
+	udp_endpoint = udp_local_endpoint ;
+}
+
+//! tcp protocol support check
+//! @return tcp support is true
+//! @return tcp not-support is false
+bool	schedule_module_round_robin::is_tcp(){ return true; }
+
+//! udp protocol support check
+//! @return udp support is true
+//! @return udp not-support is false
+bool	schedule_module_round_robin::is_udp(){ return true; }
+
+//! handle schedule called then schedule function for TCP/IP endpoint
+//! @param[in]	thread id
+//! @param[in]	list iterator first function object
+//!	@param[in]	list iterator last function object
+//!	@param[in]	list iterator next function object
+//! @param[out]	scheduled TCP/IP endpoint
+void	schedule_module_round_robin::handle_schedule(
+							boost::thread::id				thread_id,
+							rslist_iterator_func_type		inlist_begin,
+							rslist_iterator_func_type		inlist_end,
+							rslist_iterator_func_type		inlist_next,
+							boost::asio::ip::tcp::endpoint&	outendpoint ){
+	boost::asio::ip::tcp::endpoint	tcp_local_endpoint ;
+	rslist_type::iterator itr;
+
+	//! set clear data as NULL
+	outendpoint = tcp_local_endpoint;
+
+	if ( inlist_begin.empty() || inlist_end.empty() ){
+		//! invalid iterator function
+		return;
+	}
+
+	for ( itr = inlist_begin(); itr != inlist_end(); itr++ ){
+		//! keep first data of list
+		if ( itr->weight > 0 ){
+			outendpoint = itr->tcp_endpoint;
+			break;
+		}
+	}
+	if ( itr == inlist_end() )
+	{
+		//! no data
+		return;
+	}
+
+	//! first time
+	if ( tcp_local_endpoint == tcp_endpoint ){
+		//! set first data
+		tcp_endpoint = outendpoint;
+		return;
+	}
+
+	for ( ; itr != inlist_end(); itr++ ){
+		if ( itr->weight > 0 ){
+			//! prev endpoint
+			if ( tcp_endpoint == itr->tcp_endpoint ){
+				itr++;
+				break;
+			}
+		}
+	}
+	for ( ; itr != inlist_end(); itr++ ){
+		if ( itr->weight > 0 ){
+			//! set found data
+			outendpoint = itr->tcp_endpoint;
+			break ;
+		}
+	}
+
+	//! set found or first data
+	tcp_endpoint = outendpoint;
+}
+
+//! handle schedule calles then schedule function for UDP endpoint
+//! @param[in]	thread id
+//! @param[in]	list iterator first function object
+//!	@param[in]	list iterator last function object
+//!	@param[in]	list iterator next function object
+//! @param[out]	scheduled UDP endpoint
+void	schedule_module_round_robin::handle_schedule(
+							boost::thread::id				thread_id,
+							rslist_iterator_func_type		inlist_begin,
+							rslist_iterator_func_type		inlist_end,
+							rslist_iterator_func_type		inlist_next,
+							boost::asio::ip::udp::endpoint&	outendpoint ){
+	boost::asio::ip::udp::endpoint	udp_local_endpoint ;
+	rslist_type::iterator itr;
+
+	//! set clear data as NULL
+	outendpoint = udp_local_endpoint;
+
+	if ( inlist_begin.empty() || inlist_end.empty() ){
+		//! invalid iterator function
+		return;
+	}
+
+	for ( itr = inlist_begin(); itr != inlist_end(); itr++ ){
+		//! keep first data of list
+		if ( itr->weight > 0 ){
+			outendpoint = itr->udp_endpoint;
+			break;
+		}
+	}
+	if ( itr == inlist_end() )
+	{
+		//! no data
+		return;
+	}
+
+	//! first time
+	if ( udp_local_endpoint == udp_endpoint ){
+		//! set first data
+		udp_endpoint = outendpoint;
+		return;
+	}
+
+	for ( ; itr != inlist_end(); itr++ ){
+		if ( itr->weight > 0 ){
+			//! prev endpoint
+			if ( udp_endpoint == itr->udp_endpoint ){
+				itr++;
+				break;
+			}
+		}
+	}
+	for ( ; itr != inlist_end(); itr++ ){
+		if ( itr->weight > 0 ){
+			//! set found data
+			outendpoint = itr->udp_endpoint;
+			break ;
+		}
+	}
+
+	//! set found or first data
+	udp_endpoint = outendpoint;
+}
+
+//! replication interval interrrupt
+//! timer thread call this function. from virtualservice.
+void	schedule_module_round_robin::replication_interrupt(){}
+
+}	//namespace l7vs
+
+extern "C" l7vs::schedule_module_base*
+create_module(){
+	return dynamic_cast<l7vs::schedule_module_base*>(new l7vs::schedule_module_round_robin());
+}
+
+extern "C" void
+destroy_module( l7vs::schedule_module_base* in ){
+	delete in;
+}
--- unit_tests/schedule_module_test/schedule_module_rr_test.cpp
+++ unit_tests/schedule_module_test/schedule_module_rr_test.cpp
@@ -1,10 +1,8 @@
 #include <iostream>
 #include <dlfcn.h>
 #include <boost/test/included/unit_test.hpp>
+#include <boost/function.hpp>
 #include "schedule_module_base.h"
-
-//class l7vs::module_base;
-//class l7vs::schedule_module_base;
 
 #include "../../module/schedule/schedule_module_rr.cpp"
 
@@ -31,47 +29,19 @@ public:
 }	//namespace l7vs
 
 
+//l7vs::schedule_module_base::rslist_type::iterator	list_begin( l7vs::schedule_module_base::rslist_type& list );
+//l7vs::schedule_module_base::rslist_type::iterator	list_end( l7vs::schedule_module_base::rslist_type& list );
+l7vs::schedule_module_base::rslist_type::iterator	list_begin( l7vs::schedule_module_base::rslist_type* list );
+l7vs::schedule_module_base::rslist_type::iterator	list_end( l7vs::schedule_module_base::rslist_type* list );
+
 //test case1.
 void	schedule_module_test(){
-	l7vs::schedule_module_base* schedule_module_rr = NULL ;
+	l7vs::schedule_module_base* schedule_module_rr = NULL;
 
-#if	0 // SO Load
-	void*						p_so = dlopen( "./schedule_module_rr.so", RTLD_LAZY );
-	l7vs::schedule_module_base*	( *p_create )( void ) ;
-	void						( *p_destroy )( l7vs::schedule_module_base* ) ;
-
-	if ( NULL != p_so )
-	{
-		dlerror();
-		p_create = ( l7vs::schedule_module_base* (*)( void ) )dlsym( p_so, "create_module" ) ;
-		p_destroy = ( void (*)( l7vs::schedule_module_base* ) )dlsym( p_so, "destroy_module" ) ;
-
-		// unit_t_e_s_t[1]  インスタンス作成のテスト
-		try{
-			schedule_module_rr = p_create() ;
-		}
-		catch(...){
-			BOOST_ERROR( "exception : create_module" );
-		}
-		BOOST_CHECK( NULL != schedule_module_rr );
-
-		// unit_t_e_s_t[2]  get_nameのテスト
-		BOOST_CHECK_EQUAL( CHECK_NAME, schedule_module_rr->get_name() );
-
-		if ( schedule_module_rr )
-		{
-			p_destroy ( schedule_module_rr ) ;
-		}
-		dlclose ( p_so ) ;
-	}
-	else
-	{
-		BOOST_ERROR( "exception : dlopen" );
-	}
-#else
 	// unit_test[1]  インスタンス作成のテスト
+	BOOST_MESSAGE( "unit_test[1]\n" );
 	try{
-		schedule_module_rr = create_module() ;
+		schedule_module_rr = create_module();
 	}
 	catch(...){
 		BOOST_ERROR( "exception : create_module" );
@@ -79,11 +49,15 @@ void	schedule_module_test(){
 	BOOST_CHECK( NULL != schedule_module_rr );
 
 	// unit_test[2]  get_nameメソッドのテスト
+	BOOST_MESSAGE( "unit_test[2]\n" );
 	BOOST_CHECK_EQUAL( CHECK_NAME, schedule_module_rr->get_name() );
 
 	// unit_test[3]  initializeメソッドのテスト
+	BOOST_MESSAGE( "unit_test[3]\n" );
 	schedule_module_rr->initialize();
 
+	// unit_test[4]  initializeメソッドのテスト２
+	BOOST_MESSAGE( "unit_test[4]\n" );
 	// 派生クラスにgetter/setterを追加してprotected変数の確認
 	l7vs::schedule_module_round_robin_fake	schedule_module_rr_fake ;
 	BOOST_CHECK_EQUAL( CHECK_NAME, schedule_module_rr_fake.get_name() );
@@ -102,15 +76,166 @@ void	schedule_module_test(){
 	BOOST_CHECK( schedule_module_rr_fake.get_udp_endpoint() != endpoint2 );
 
 
-	// unit_test[4]  is_tcpメソッドのテスト
+	// unit_test[5]  is_tcpメソッドのテスト
+	BOOST_MESSAGE( "unit_test[5]\n" );
 	BOOST_CHECK_EQUAL( true, schedule_module_rr->is_tcp() );
 
-	// unit_test[5]  is_udpメソッドのテスト
+	// unit_test[6]  is_udpメソッドのテスト
+	BOOST_MESSAGE( "unit_test[6]\n" );
 	BOOST_CHECK_EQUAL( true, schedule_module_rr->is_udp() );
 
+	boost::thread::id								thread_id;
+	l7vs::schedule_module_base::rslist_type		rs_list;
+	l7vs::realserver	server1, server2, server3;
+
+	l7vs::schedule_module_base::rslist_iterator_func_type		rslist_begin;
+	l7vs::schedule_module_base::rslist_iterator_func_type		rslist_end;
+	l7vs::schedule_module_base::rslist_iterator_func_type		rslist_next;
+
+	// unit_test[7]  handle_schedule(tcp)メソッドのテスト
+	BOOST_MESSAGE( "unit_test[7]\n" );
+	boost::asio::ip::tcp::endpoint endpoint3 ;
+
+	endpoint1 = endpoint3 ;
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( endpoint3 == endpoint1 );
+
+	// unit_test[8]  handle_schedule(tcp)メソッドのテスト２
+	BOOST_MESSAGE( "unit_test[8]\n" );
+	rslist_begin = boost::bind( &list_begin, &rs_list );
+	rslist_end = boost::bind( &list_end, &rs_list );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( endpoint3 == endpoint1 );
+
+	// unit_test[9]  handle_schedule(tcp)メソッドのテスト３
+	BOOST_MESSAGE( "unit_test[9]\n" );
+	server1.tcp_endpoint = boost::asio::ip::tcp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.87" ), 22  ) ;
+	server1.weight = 0;
+	rs_list.push_back( server1 );
+	server2.tcp_endpoint = boost::asio::ip::tcp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.86" ), 21  ) ;
+	server2.weight = 0;
+	rs_list.push_back( server2 );
+	server3.tcp_endpoint = boost::asio::ip::tcp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.85" ), 20  ) ;
+	server3.weight = 0;
+	rs_list.push_back( server3 );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( endpoint3 == endpoint1 );
+
+	// unit_test[10]  handle_schedule(tcp)メソッドのテスト４
+	BOOST_MESSAGE( "unit_test[10]\n" );
+	rs_list.clear();
+	server1.weight = 2;
+	rs_list.push_back( server1 );
+	server2.weight = 1;
+	rs_list.push_back( server2 );
+	server3.weight = 3;
+	rs_list.push_back( server3 );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( server1.tcp_endpoint == endpoint1 );
+
+	// unit_test[11]  handle_schedule(tcp)メソッドのテスト５
+	BOOST_MESSAGE( "unit_test[11]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( server2.tcp_endpoint == endpoint1 );
+
+	// unit_test[12]  handle_schedule(tcp)メソッドのテスト６
+	BOOST_MESSAGE( "unit_test[12]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( server3.tcp_endpoint == endpoint1 );
+
+	// unit_test[13]  handle_schedule(tcp)メソッドのテスト７
+	BOOST_MESSAGE( "unit_test[13]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint1 ) ;
+	BOOST_CHECK( server1.tcp_endpoint == endpoint1 );
+
+
+	// unit_test[14]  handle_schedule(udp)メソッドのテスト
+	BOOST_MESSAGE( "unit_test[14]\n" );
+	boost::asio::ip::udp::endpoint endpoint4 ;
+
+	endpoint2 = endpoint4 ;
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( endpoint4 == endpoint2 );
+
+	// unit_test[15]  handle_schedule(udp)メソッドのテスト２
+	BOOST_MESSAGE( "unit_test[15]\n" );
+//	rslist_begin = boost::bind( &list_begin, &rs_list );
+//	rslist_end = boost::bind( &list_end, &rs_list );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( endpoint4 == endpoint2 );
+
+	// unit_test[16]  handle_schedule(udp)メソッドのテスト３
+	BOOST_MESSAGE( "unit_test[16]\n" );
+	rs_list.clear();
+	server1.udp_endpoint = boost::asio::ip::udp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.87" ), 22  ) ;
+	server1.weight = 0;
+	rs_list.push_back( server1 );
+	server2.udp_endpoint = boost::asio::ip::udp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.86" ), 21  ) ;
+	server2.weight = 0;
+	rs_list.push_back( server2 );
+	server3.udp_endpoint = boost::asio::ip::udp::endpoint ( boost::asio::ip::address::from_string( "10.144.169.85" ), 20  ) ;
+	server3.weight = 0;
+	rs_list.push_back( server3 );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( endpoint4 == endpoint2 );
+
+	// unit_test[17]  handle_schedule(udp)メソッドのテスト４
+	BOOST_MESSAGE( "unit_test[17]\n" );
+	rs_list.clear();
+	server1.weight = 2;
+	rs_list.push_back( server1 );
+	server2.weight = 1;
+	rs_list.push_back( server2 );
+	server3.weight = 3;
+	rs_list.push_back( server3 );
+
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( server1.udp_endpoint == endpoint2 );
+
+	// unit_test[18]  handle_schedule(udp)メソッドのテスト５
+	BOOST_MESSAGE( "unit_test[18]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( server2.udp_endpoint == endpoint2 );
+
+	// unit_test[19]  handle_schedule(udp)メソッドのテスト６
+	BOOST_MESSAGE( "unit_test[19]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( server3.udp_endpoint == endpoint2 );
+
+	// unit_test[20]  handle_schedule(udp)メソッドのテスト７
+	BOOST_MESSAGE( "unit_test[20]\n" );
+	schedule_module_rr->handle_schedule( thread_id, rslist_begin, rslist_end, rslist_next, endpoint2 ) ;
+	BOOST_CHECK( server1.udp_endpoint == endpoint2 );
+
+
+	// unit_test[21]  replication_interruptメソッドのテスト
+	BOOST_MESSAGE( "unit_test[20]\n" );
+	schedule_module_rr->replication_interrupt();
 
 	destroy_module( schedule_module_rr );
-#endif
+}
+
+//l7vs::schedule_module_base::rslist_type::iterator	list_begin( l7vs::schedule_module_base::rslist_type& list ){
+//	return(list.begin());
+//}
+
+//l7vs::schedule_module_base::rslist_type::iterator	list_end( l7vs::schedule_module_base::rslist_type& list ){
+//	return(list.end());
+//}
+
+l7vs::schedule_module_base::rslist_type::iterator	list_begin( l7vs::schedule_module_base::rslist_type* list ){
+	return(list->begin());
+}
+
+l7vs::schedule_module_base::rslist_type::iterator	list_end( l7vs::schedule_module_base::rslist_type* list ){
+	return(list->end());
 }
 
 test_suite*	init_unit_test_suite( int argc, char* argv[] ){
