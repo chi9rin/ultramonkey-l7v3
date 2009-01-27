429fe9eced2236d3129b67133dfe1ea175fb83ac
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 11:00:30 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6393 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/tcp_thread_message_que.h
+++ include/tcp_thread_message_que.h
@@ -1,4 +1,14 @@
- 
+//
+//!	@file	tcp_thread_message_que.h
+//!	@brief	tcp session thread message queue class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef TCP_THREAD_MESSAGE_QUE_H
 #define TCP_THREAD_MESSAGE_QUE_H
 
@@ -6,24 +16,37 @@
 #include <queue>
 #include <boost/thread/mutex.hpp>
 
-
-//#include "tcp_thread_message.h"
-
 namespace l7vs{
+//!	@class	tcp_thread_message
+//! @brief	tcp_thread_message class name define.
 	class tcp_thread_message;
 	
+//!	@class	data_buff_base
+//! @brief	this class is tcp_thread_message object queue.
 	class tcp_thread_message_que : private boost::noncopyable{
-		typedef boost::shared_ptr<tcp_thread_message> tcp_thread_message_ptr;
 		public:
+			//!	queue message type
+			typedef boost::shared_ptr<tcp_thread_message> tcp_thread_message_ptr;
+			
+			//! construcor
+			tcp_thread_message_que();
+			//! destructor
+			~tcp_thread_message_que();
+			//! push queue
+			//! @param[in]	push message
+			void push(tcp_thread_message_ptr message);
+			//! front queue
+			tcp_thread_message_ptr front();
+			//! queue check empty
+			bool empty();
+			//! clear queue
+			void clear();
+		protected:
+			//! message queue
 			std::queue< tcp_thread_message_ptr > message_que;
+			//! queue access mutex
 			boost::mutex que_mutex;
 			
-			tcp_thread_message_que();
-			~tcp_thread_message_que();
-			void push(tcp_thread_message_ptr message);
-			tcp_thread_message_ptr front();
-			bool empty();
-			void clear();
 	};// class tcp_thread_message_que
 }// namespace l7vs
 
