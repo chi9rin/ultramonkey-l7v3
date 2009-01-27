35e1955a49d33d6cb363ca04b7f6478e805cdc48
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 01:32:56 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6355 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/session_result_message.h
+++ include/session_result_message.h
@@ -1,16 +1,38 @@
+//
+//!	@file	session_result_message.h
+//!	@brief	session result message class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef SESSION_RESULT_MESSAGE_H
 #define SESSION_RESULT_MESSAGE_H
 
 #include <string>
 
 namespace l7vs{
+//!	@struct	session_result_message
+//! @brief	this struct is session class use result message.
 	struct session_result_message{
+		//! result flag
 		bool flag;
+		//! message
 		std::string message;
-
+		//! == operator
+		//! @param[in]	equal check object.
+		//! @return 	true is equal
+		//! @return 	false is not equal
 		bool operator==(const session_result_message& in){
 			return ((flag == in.flag) && (message == in.message));
 		}
+		//! != operator
+		//! @param[in]	inequal check object.
+		//! @return 	true is inequal
+		//! @return 	false is not inequal
 		bool operator!=(const session_result_message& in){
 			return ((flag != in.flag) || (message != in.message));
 		}
