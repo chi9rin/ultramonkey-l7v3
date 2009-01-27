3b0e7d52921b43386117a6a4767d7a2e7e827ebd
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 04:24:29 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6363 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/tcp_thread_message.cpp
+++ src/tcp_thread_message.cpp
@@ -1,12 +1,27 @@
- 
+//
+//!	@file	tcp_thread_message.cpp
+//!	@brief	tcp session thread message class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #include "tcp_thread_message.h"
  
 namespace l7vs{
 
+	//! construcor
 	tcp_thread_message::tcp_thread_message(){
+		
 	}
+	
+	//! destructor
 	tcp_thread_message::~tcp_thread_message(){
+		
 	}
-
+	
 }// namespace l7vs
 
