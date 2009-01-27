e3d3ffb4e7cc95a7ab258f7cf9887f7d5662e02b
yatabe <yatabe@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 09:41:46 +0000

    更新
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6383 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- module/protocol/http_protocol_module_base.h
+++ module/protocol/http_protocol_module_base.h
@@ -9,11 +9,12 @@
 #ifndef	HTTP_PROTOCOL_MODULE_BASE_H
 #define	HTTP_PROTOCOL_MODULE_BASE_H
 
-#include "protocol_module_base.h"
+//#include "protocol_module_base.h"
 
 namespace l7vs{
 
-class http_protocol_module_base : public protocol_module_base {
+//class http_protocol_module_base : public protocol_module_base {
+class http_protocol_module_base {
 protected:
 	//! @enum	CHECK_RESULT_TAG
 	//! @brief	check tag is return to http protocol module.
@@ -27,31 +28,31 @@ protected:
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
 	//! @return CHECK_RESULT_TAG	http method is valid
-	CHECK_RESULT_TAG	check_http_method(	const char*, const size_t ) const;
+	CHECK_RESULT_TAG	check_http_method( char*, const size_t ) const;
 	//! check http version function
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
 	//! @return	CHECK_RESULT_TAG 	http version 1.0 or 1.1
-	CHECK_RESULT_TAG	check_http_version(	const char*, const size_t ) const;
+	CHECK_RESULT_TAG	check_http_version( char*, const size_t ) const;
 	//! check http status code function
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
 	//! @return	CHECK_RESULT_TAG	status code is nomal or error
-	CHECK_RESULT_TAG	check_status_code(	const char*, const size_t ) const;
+	CHECK_RESULT_TAG	check_status_code( char*, const size_t ) const;
 	//! serch uri function
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
 	//! @param size_t&				uri offset
 	//! @param size_t&				uri length
 	//! @return bool				find is true. not find is false
-	bool	find_uri(	const char*, const size_t, size_t&, size_t&);
+	bool	find_uri( char*, const size_t, size_t&, size_t&);
 	//! serch status function
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
 	//! @param size_t&				status offset
 	//! @param size_t&				status length
 	//! @return bool				find is true. not find is false
-	bool	find_status_code(	const char*, const size_t, size_t&, size_t& );
+	bool	find_status_code( char*, const size_t, size_t&, size_t& );
 	//! serch http header
 	//! @param const char*			buffer
 	//! @param const size_t			buffer_len
@@ -59,11 +60,12 @@ protected:
 	//! @param size_t&				header offset
 	//! @param size_t&				header length
 	//! @return bool				find is true. not find is false
-	bool	find_http_header(	const char*, const size_t, const std::string&, size_t&, size_t& );
+	bool	find_http_header( char*, const size_t, const std::string&, size_t&, size_t& );
 
 public:
 	//! constractor
-	http_protocol_module_base( std::string in_modulename ) : protocol_module_base( in_modulename ){};
+//	http_protocol_module_base( std::string in_modulename ) : protocol_module_base( in_modulename ){};
+	http_protocol_module_base( std::string in_modulename ){};
 	//! destractor
 	virtual	~http_protocol_module_base(){};
 };
