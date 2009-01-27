48b0a92b9a61c855920aded56daa7fdc89582225
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-27 04:23:57 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6362 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/tcp_thread_message.h
+++ include/tcp_thread_message.h
@@ -1,4 +1,13 @@
- 
+//
+//!	@file	tcp_thread_message.h
+//!	@brief	tcp session thread message class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
 
 #ifndef TCP_THREAD_MESSAGE_H
 #define TCP_THREAD_MESSAGE_H
@@ -10,13 +19,20 @@
 
 namespace l7vs{
 
+//!	@class	tcp_thread_message
+//! @brief	this class is notify message in tcp session thread.
 	class tcp_thread_message : private boost::noncopyable{
 		public:
+			//! construcor 
+			tcp_thread_message();
+			//! destructor
+			~tcp_thread_message();
+			
+			//! request call function object
 			boost::function<void(tcp_session::TCP_PROCESS_TYPE_TAG)> message;
+			//! function require endpoint 
 			boost::asio::ip::tcp::endpoint endpoint_info;
 			
-			tcp_thread_message();
-			~tcp_thread_message();
 	};// class tcp_thread_message
 }// namespace l7vs
 
