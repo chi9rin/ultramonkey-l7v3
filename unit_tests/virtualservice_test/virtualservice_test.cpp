b9ac4632581bdf2aba288cc00cbeab659130d550
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-28 05:25:36 +0000

    テスト内容追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6411 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/virtualservice_test/virtualservice_test.cpp
+++ unit_tests/virtualservice_test/virtualservice_test.cpp
@@ -6,21 +6,27 @@
 using namespace boost::unit_test;
 
 //test case1.
-void	virtualservice_tcp_test(){
+void	virtualservice_base_test1(){
+	// unit_test[1]  object create
+	BOOST_MESSAGE( "-------1" );
+	
+	// unit_test[2]  object destroy
+	BOOST_MESSAGE( "-------2" );
 }
 
 //test case2
-void	virtualservice_udp_test(){
+void	virtualservice_base_test2(){
 }
+
 
 test_suite*	init_unit_test_suite( int argc, char* argv[] ){
 
 	// create unit test suite
-	test_suite* ts = BOOST_TEST_SUITE( "virtualservice_test" );
+	test_suite* ts = BOOST_TEST_SUITE( "virtualservice_base_test" );
 
 	// add test case to test suite
-	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test ) );
-	ts->add( BOOST_TEST_CASE( &virtualservice_udp_test ) );
+	ts->add( BOOST_TEST_CASE( &virtualservice_base_test1 ) );
+	ts->add( BOOST_TEST_CASE( &virtualservice_base_test2 ) );
 
 	framework::master_test_suite().add( ts );
 
