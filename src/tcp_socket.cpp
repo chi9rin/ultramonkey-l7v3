3ccb8895efa8a231ece572c7555bcd7acbd21462
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-30 01:32:43 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6463 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/tcp_socket.cpp
+++ src/tcp_socket.cpp
@@ -1,58 +1,116 @@
+//
+//!	@file	tcp_socket.cpp
+//!	@brief	tcp session socket class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
+#include <boost/thread/thread.hpp>
+
 #include "tcp_socket.h"
+#include "logger.h"
 
 namespace l7vs{
-	
+
+	//! construcor
 	tcp_socket::tcp_socket(boost::asio::io_service& io):
 		my_socket(io){
-		
-	}
-	
-	tcp_socket::~tcp_socket(){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::tcp_socket", __FILE__, __LINE__ );
+
 	}
 
+	//! destructor
+	tcp_socket::~tcp_socket(){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::~tcp_socket", __FILE__, __LINE__ );
+	}
+
+	//! get reference control socket
+	//! @return			reference control socket
 	boost::asio::ip::tcp::socket& tcp_socket::get_socket(){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::get_socket", __FILE__, __LINE__ );
 		return my_socket;
 	}
 
+	//! connect socket
+	//! @param[in]		connect_endpoint is connection endpoint
+	//! @param[out]		ec is reference error code object
+	//! @return 		true is connect
+	//! @return 		false is connect failure 
 	bool tcp_socket::connect(boost::asio::ip::tcp::endpoint connect_endpoint,
 		boost::system::error_code& ec){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::connect", __FILE__, __LINE__ );
+			
+		boost::mutex::scoped_lock scope_lock(socket_mutex);
+		my_socket.connect(connect_endpoint,ec);
+		if(!ec){
+			open_flag = true;
+		}else{
+			open_flag = false;
+		}
+			
+		return open_flag;
+	}
+
+	//! close socket
+	//! @param[out]		ec is reference error code object
+	//! @return 		true is socket close
+	//! @return 		false is not open socket
+	bool tcp_socket::close(boost::system::error_code& ec){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::close", __FILE__, __LINE__ );
+		
+		boost::mutex::scoped_lock scope_lock(socket_mutex);
+		my_socket.close(ec);
+		bool bres = false;
+		if(open_flag){
+			open_flag = false;
+			bres = true;
+		}
+		
+		return bres;
+	}
+
+	//! set non blocking mode of the socket 
+	//! @return			ec is reference error code object
+	bool tcp_socket::set_non_blocking_mode(boost::system::error_code& ec){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::set_non_blocking_mode", __FILE__, __LINE__ );
+		
+		boost::asio::socket_base::non_blocking_io cmd(true);
+		my_socket.io_control(cmd,ec);
+		
 		return true;
 	}
 
-	bool tcp_socket::close(boost::system::error_code& ec){
-		return true;
-	}
-	bool tcp_socket::set_non_blocking_mode(boost::system::error_code& ec){
-		return true;
-	}
-	
+	//! write socket
+	//! @param[in]		buffers is wite data buffer
+	//! @param[out]		ec is reference error code object
+	//! @return			write data size	
 	std::size_t tcp_socket::write_some(boost::asio::mutable_buffers_1 buffers,
-	boost::system::error_code& ec){
-		/*				
-		const void* pdata = boost::asio::detail::buffer_cast_helper(*buffers.begin());
-		char* psz = (char*)pdata;
-		std::cout << "<";
-		std::cout << psz;
-		std::cout << ">";
-		std::cout << "write_some OK!\n";
-		*/
-		std::size_t size = 0;
-		return size;
-	}
+		boost::system::error_code& ec){
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::write_some", __FILE__, __LINE__ );
+
+		std::size_t res_size = 0;
+		res_size = my_socket.write_some(buffers,ec);
 		
+		return res_size;
+	}
+
+	//! read socket
+	//! @param[out]		buffers is read data buffer
+	//! @param[out]		ec is reference error code object
+	//! @return			read data size
 	std::size_t tcp_socket::read_some(boost::asio::mutable_buffers_1 buffers,
 		boost::system::error_code& ec){
-			/*	
-		const void* pdata = boost::asio::detail::buffer_cast_helper(*buffers.begin());
-		char* psz = (char*)pdata;
-		std::cout << "<";
-		std::cout << psz;
-		std::cout << ">";
-		std::cout << "read_some OK!\n";
-			*/
-		std::size_t size = 0;
-		return size;
+		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_socket::read_some", __FILE__, __LINE__ );
+			
+		std::size_t res_size = 0;
+		res_size = my_socket.read_some(buffers,ec);
+		
+		return res_size;
 	}
-	
+
 }// namespace l7vs
  
