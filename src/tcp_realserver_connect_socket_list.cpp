c4d9b88aed37e0891b2257668f4e0a30bc30647c
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-30 05:51:45 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6490 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/tcp_realserver_connect_socket_list.cpp
+++ src/tcp_realserver_connect_socket_list.cpp
@@ -16,18 +16,15 @@ namespace l7vs{
 	
 	//! construcor
 	tcp_realserver_connect_socket_list::tcp_realserver_connect_socket_list(){
-		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_realserver_connect_socket_list::tcp_realserver_connect_socket_list", __FILE__, __LINE__ );
 	}
 	//! destructor
 	tcp_realserver_connect_socket_list::~tcp_realserver_connect_socket_list(){
-		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_realserver_connect_socket_list::~tcp_realserver_connect_socket_list", __FILE__, __LINE__ );
 	}
 	
 	//! push back list
 	//! @param[in/out]	realserver_socket is realserver list push back element 
 	void tcp_realserver_connect_socket_list::push_back(list_element realserver_socket){		
 		boost::mutex::scoped_lock scope_lock(list_mutex);
-		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_realserver_connect_socket_list::push_back", __FILE__, __LINE__ );
 		
 		connect_list.push_back(realserver_socket);
 	}
@@ -36,17 +33,18 @@ namespace l7vs{
 	//! @return			realserver list element
 	tcp_realserver_connect_socket_list::list_element tcp_realserver_connect_socket_list::get_socket(){
 		boost::mutex::scoped_lock scope_lock(list_mutex);
-		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_realserver_connect_socket_list::get_socket", __FILE__, __LINE__ );
 		
-		list_element& sock = *(connect_list.begin());
-		connect_list.pop_front();
+		list_element sock;
+		if(!connect_list.empty()){
+			sock = *(connect_list.begin());
+			connect_list.pop_front();
+		}
 		return sock;
 	}
 	
 	//! check empty list
 	bool tcp_realserver_connect_socket_list::empty(){
 		boost::mutex::scoped_lock scope_lock(list_mutex);
-		Logger	logger( LOG_CAT_L7VSD_SESSION, 9999, "tcp_realserver_connect_socket_list::empty", __FILE__, __LINE__ );
 		
 		bool bres = connect_list.empty();
 		return bres;
