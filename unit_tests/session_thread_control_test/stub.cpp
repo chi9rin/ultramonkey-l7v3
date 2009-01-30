2ff7ec3e5e6bdc64d7efaa8350f42268853f230d
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-30 05:18:40 +0000

    LoggerのStubを追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6485 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/session_thread_control_test/stub.cpp
+++ unit_tests/session_thread_control_test/stub.cpp
@@ -9,6 +9,52 @@
 #include "udp_session.h"
 
 
+l7vs::Logger::Logger() :
+	scopedLogCategory(LOG_CAT_L7VSD_LOGGER),
+	scopedLogId(0),
+	scopedLogMessage("Logger Constructor"),
+	scopedLogFile(__FILE__),
+	scopedLogLine(__LINE__) {}
+
+l7vs::Logger::Logger( LOG_CATEGORY_TAG cat, const unsigned int id, const std::string& msg, const char* file, int line) :
+	scopedLogCategory(cat),
+	scopedLogId(id),
+	scopedLogMessage(msg),
+	scopedLogFile(file),
+	scopedLogLine(line) {}
+
+l7vs::Logger::~Logger(){}
+
+void	l7vs::Logger::loadConf(){}
+
+l7vs::LOG_LEVEL_TAG l7vs::Logger::getLogLevel( l7vs::LOG_CATEGORY_TAG ){
+	l7vs::LOG_LEVEL_TAG		ret_tag = l7vs::LOG_LV_DEBUG;
+	return ret_tag;
+}
+
+bool	l7vs::Logger::setLogLevel( l7vs::LOG_CATEGORY_TAG cat, l7vs::LOG_LEVEL_TAG level ){
+}
+
+void	l7vs::Logger::putLogFatal( l7vs::LOG_CATEGORY_TAG, const unsigned int, const std::string& msg, const char*, int ){
+	std::cout << "FATAL : " << msg << std::endl;
+}
+
+void	l7vs::Logger::putLogError( l7vs::LOG_CATEGORY_TAG, const unsigned int, const std::string& msg, const char*, int ){
+	std::cout << "ERROR : " << msg << std::endl;
+}
+
+void	l7vs::Logger::putLogWarn( l7vs::LOG_CATEGORY_TAG, const unsigned int, const std::string& msg, const char*, int ){
+	std::cout << "WARN  : " << msg << std::endl;
+}
+
+void	l7vs::Logger::putLogInfo( l7vs::LOG_CATEGORY_TAG, const unsigned int, const std::string& msg, const char*, int ){
+	std::cout << "INFO  : " << msg << std::endl;
+}
+
+void	l7vs::Logger::putLogDebug( l7vs::LOG_CATEGORY_TAG, const unsigned int, const std::string& msg, const char*, int ){
+	std::cout << "DEBUG : " << msg << std::endl;
+}
+
 l7vs::l7vsd::l7vsd(){}
 l7vs::l7vsd::~l7vsd(){}
 l7vs::l7vsd::vslist_type::iterator	l7vs::l7vsd::search_vslist( const virtualservice_element& )	const{
