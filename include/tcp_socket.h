8f540dab5aa792f39ba3be45584794be3ccae031
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-30 01:31:40 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6462 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/tcp_socket.h
+++ include/tcp_socket.h
@@ -1,3 +1,14 @@
+//
+//!	@file	tcp_socket.h
+//!	@brief	tcp session socket class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef TCP_SOCKET_H
 #define TCP_SOCKET_H
 
@@ -6,20 +17,49 @@
 
 namespace l7vs{
 
+//!	@class	tcp_socket
+//! @brief	this class is tcp session object use socket.
 	class tcp_socket : private boost::noncopyable{
-		protected:
-			boost::asio::ip::tcp::socket my_socket;
-			boost::mutex socket_mutex;
-			bool open_flag;
 		public:
+
+			//! construcor
+			//! @param[in/out]	socket use io service object
 			tcp_socket(boost::asio::io_service& io);
+			//! destructor
 			~tcp_socket();
+			//! get reference control socket
+			//! @return			reference control socket
 			boost::asio::ip::tcp::socket& get_socket();
+			//! connect socket
+			//! @param[in]		connect_endpoint is connection endpoint
+			//! @param[out]		ec is reference error code object
 			bool connect(const boost::asio::ip::tcp::endpoint connect_endpoint,boost::system::error_code& ec);
+			//! close socket
+			//! @param[out]		ec is reference error code object
+			//! @return 		true is socket close
+			//! @return 		false is not open socket
 			bool close(boost::system::error_code& ec);
+			//! set non blocking mode of the socket 
+			//! @return			ec is reference error code object
 			bool set_non_blocking_mode(boost::system::error_code& ec);
+			//! write socket
+			//! @param[in]		buffers is wite data buffer
+			//! @param[out]		ec is reference error code object
+			//! @return			write data size
 			std::size_t write_some(boost::asio::mutable_buffers_1 buffers, boost::system::error_code& ec);
+			//! read socket
+			//! @param[out]		buffers is read data buffer
+			//! @param[out]		ec is reference error code object
+			//! @return			read data size
 			std::size_t read_some(boost::asio::mutable_buffers_1 buffers, boost::system::error_code& ec);
+
+		protected:
+			//! control socket
+			boost::asio::ip::tcp::socket my_socket;
+			//! socket close mutex
+			boost::mutex socket_mutex;
+			//! socket open flag
+			bool open_flag;
 
 	};// class tcp_socket
 }// namespace l7vs
