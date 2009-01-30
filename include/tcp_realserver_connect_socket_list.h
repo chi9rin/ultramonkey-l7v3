38c9545b49420594f314ab1bb5ded124086c6c33
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-01-30 05:52:41 +0000

    UT完了
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel@6491 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/tcp_realserver_connect_socket_list.h
+++ include/tcp_realserver_connect_socket_list.h
@@ -1,3 +1,14 @@
+//
+//!	@file	tcp_realserver_connect_socket_list.h
+//!	@brief	tcp session connect realserver socket list class
+//
+//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
+//	mail: 
+//
+//	Distributed under the Boost Software License, Version 1.0.(See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
+//
+
 #ifndef TCP_REALSERVER_CONNECT_SOCKET_LIST_H
 #define TCP_REALSERVER_CONNECT_SOCKET_LIST_H
 
@@ -9,19 +20,33 @@
 
 namespace l7vs{
 
-	class tcp_realserver_connect_socket_list : 
-			private boost::noncopyable{
-		typedef boost::shared_ptr<tcp_socket> tcp_socket_ptr;
-		typedef std::pair<boost::asio::ip::tcp::endpoint,tcp_socket_ptr > list_element;
-		protected:
-			std::list< list_element > connect_list;
-			boost::mutex list_mutex;
+//!	@class	tcp_realserver_connect_socket_list
+//! @brief	this class is connected realsever list by up thread.
+	class tcp_realserver_connect_socket_list : private boost::noncopyable{
 		public:
+			//!	socket pointer type
+			typedef boost::shared_ptr<tcp_socket> tcp_socket_ptr;
+			//! realserver list element type first : connection endpoint second : socket pointer
+			typedef std::pair<boost::asio::ip::tcp::endpoint,tcp_socket_ptr > list_element;
+			
+			//! construcor
 			tcp_realserver_connect_socket_list();
+			//! destructor
 			~tcp_realserver_connect_socket_list();
+			//! push back list
+			//! @param[in/out]	realserver_socket is realserver list push back element 
 			void push_back(list_element realserver_socket);
+			//! get socket list
+			//! @return			realserver list element
 			list_element get_socket();
+			//! check empty list
 			bool empty();
+			
+		protected:
+			//! realserver list
+			std::list< list_element > connect_list;
+			//! list access mutex
+			boost::mutex list_mutex;
 	};// class tcp_realserver_connect_socket_list
 }// namespace l7vs
 
