fa05efb7bbe13407a6ce37733c278fea64a1f704
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-02-23 04:11:40 +0000

    #297
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-IT@7137 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/session_test/tcp_socket/tcp_socket_test.cpp
+++ unit_tests/session_test/tcp_socket/tcp_socket_test.cpp
@@ -27,6 +27,7 @@ class test_mirror_server{
 		size_t receive_size;
 		int req_recv_cnt;
 		bool brecv_triger;
+		size_t data_size;
 		
 		test_mirror_server() : 
 				bstop_flag(false),
@@ -39,7 +40,8 @@ class test_mirror_server{
 				connect_end(boost::asio::ip::address::from_string(DUMMI_SERVER_IP), DUMMI_SERVER_PORT),
 				receive_size(0),
 				req_recv_cnt(0),
-				brecv_triger(false){
+				brecv_triger(false),
+				data_size(0){
 		};
 		
 		~test_mirror_server(){
@@ -63,6 +65,7 @@ class test_mirror_server{
 			
 			boost::array<char,MAX_BUFFER_SIZE> buf;
 			receive_size = 0;
+			size_t send_size = 0;
 			
 			
 			while(!bstop_flag){
@@ -96,21 +99,36 @@ class test_mirror_server{
 							break;
 							
 						// receive
-						receive_size = con.read_some(boost::asio::buffer(buf,MAX_BUFFER_SIZE),ec);
-						
-						if(ec){
-							if(ec == boost::asio::error::eof || ec == boost::asio::error::connection_reset){
-								std::cout << "dummy mirror server detect client disconnect!" << std::endl;
-								bdisconnect_flag = true;
-							}else{
-								std::cout << "dummy mirror server receive NG!" << std::endl;
+						receive_size = 0;
+						while(receive_size < data_size){
+							size_t ret_size = con.read_some(boost::asio::buffer(buf.data() + receive_size,data_size - receive_size),ec);
+							if(ec){
+								if(ec == boost::asio::error::eof || ec == boost::asio::error::connection_reset){
+									std::cout << "dummy mirror server detect client disconnect!" << std::endl;
+									bdisconnect_flag = true;
+									break;
+								}else{
+									std::cout << "dummy mirror server receive NG!" << std::endl;
+									break;
+								}
+							}else if(ret_size > 0){
+								receive_size += ret_size;
+								std::cout << "dummy mirror server receive " << receive_size << "Byte" << std::endl;
 							}
-						}else{
+						}
+						if(!ec){
 							if(receive_size > 0){
 								// send
-								con.write_some(boost::asio::buffer(buf,receive_size),ec);
-								if(ec){
-									std::cout << "dummy mirror server send NG!" << std::endl;
+								send_size = 0;
+								while(send_size < receive_size){
+									size_t ret_size = con.write_some(boost::asio::buffer(buf.data() + send_size,receive_size - send_size),ec);
+									if(ec){
+										std::cout << "dummy mirror server send NG!" << std::endl;
+										break;
+									}else if(ret_size > 0){
+										send_size += ret_size;
+										std::cout << "dummy mirror server send " << send_size << "Byte" << std::endl;
+									}
 								}
 							}
 						}
@@ -156,7 +174,7 @@ class test_socket_class : public l7vs::tcp_socket{
 		my_socket.close(ec);
 	}
 	
-	bool get_open_flag(){
+	bool& get_open_flag(){
 		return open_flag;
 	}
 	
@@ -178,13 +196,13 @@ void construcor_test(){
 	
 	test_socket_class test_obj(io);
 	
-	// ## construcor test [1] set io object
-	std::cout << "construcor test [1] set io object" << std::endl;
+	// unit_test [1] construcor test set io object
+	std::cout << "[1] construcor test set io object" << std::endl;
 	boost::asio::io_service& set_io = test_obj.get_io();
 	BOOST_CHECK_EQUAL(&io , &set_io);
 	
-	// ## construcor test [2] init open_flag
-	std::cout << "construcor test [2] init open_flag" << std::endl;
+	// unit_test [2] construcor test init open_flag
+	std::cout << "[2] construcor test init open_flag" << std::endl;
 	BOOST_CHECK(!test_obj.get_open_flag());
 
 	
@@ -212,8 +230,8 @@ void connect_test(){
 	
 	std::cout << "ready dummy mirror server" << std::endl;
 	
-	// ## connect test [1] connection success error_code object
-	std::cout << "connect test [1] connection success error_code object" << std::endl;
+	// unit_test [1] connect test connection success error_code object
+	std::cout << "[1] connect test connection success error_code object" << std::endl;
 	boost::asio::ip::tcp::endpoint connect_end(boost::asio::ip::address::from_string(DUMMI_SERVER_IP), DUMMI_SERVER_PORT);
 	boost::asio::io_service io;
 	boost::system::error_code ec;
@@ -221,9 +239,8 @@ void connect_test(){
 	test_obj.connect(connect_end,ec);
 	BOOST_CHECK(!ec);
 	
-	
-	// ## connect test [2] connection success open_flag
-	std::cout << "connect test [2] connection success open_flag" << std::endl;
+	// unit_test [2] connect test connection success open_flag
+	std::cout << "[2] connect test connection success open_flag" << std::endl;
 	BOOST_CHECK(test_obj.get_open_flag());
 	
 	while(!test_server.bconnect_flag){
@@ -233,32 +250,39 @@ void connect_test(){
 	boost::asio::ip::tcp::endpoint chk_end;
 	boost::asio::ip::tcp::endpoint ref_end;
 	
-	// ## connect test [3] connect local endpoint
-	std::cout << "connect test [3] connect local endpoint" << std::endl;	
+	// unit_test [3] connect test connect local endpoint
+	std::cout << "[3] connect test connect local endpoint" << std::endl;	
 	chk_end = test_obj.get_local_end();
 	ref_end = test_server.connect_end;
 	BOOST_CHECK_EQUAL(chk_end , ref_end);
 	
 	
-	// ## connect test [4] connect remote endpoint
-	std::cout << "connect test [4] connect remote endpoint" << std::endl;
+	// unit_test [4] connect test connect remote endpoint
+	std::cout << "[4] connect test connect remote endpoint" << std::endl;
 	chk_end = test_obj.get_remote_end();
 	ref_end = connect_end;
 	BOOST_CHECK_EQUAL(chk_end , ref_end);
-		
+
+	// unit_test [5] connect test connect recall check
+	std::cout << "[5] connect test connect recall check" << std::endl;
+	test_obj.connect(connect_end,ec);
+	BOOST_CHECK(!ec);
+	BOOST_CHECK(test_obj.get_open_flag());
+	
 	test_obj.test_close(ec);
+	test_obj.get_open_flag() = false;
 	
 	test_server.breq_close_wait_flag = false;	
 	test_server.bstop_flag = true;
 	server_thread.join();
 		
-	// ## connect test [5] connection faile error_code object
-	std::cout << "connect test [5] connection faile error_code object" << std::endl;
+	// unit_test [6] connect test connection faile error_code object
+	std::cout << "[6] connect test connection faile error_code object" << std::endl;
 	test_obj.connect(connect_end,ec);
 	BOOST_CHECK(ec);
 		
-	// ## connect test [6] connection faile open_flag
-	std::cout << "connect test [6] connection faile open_flag" << std::endl;
+	// unit_test [7] connect test connection faile open_flag
+	std::cout << "[7] connect test connection faile open_flag" << std::endl;
 	BOOST_CHECK(!test_obj.get_open_flag());
 	
 	BOOST_MESSAGE( "----- connect test end -----" );
@@ -266,7 +290,7 @@ void connect_test(){
 }
 
 void set_non_blocking_mode_test(){
-	BOOST_MESSAGE( "----- set non blocking mode test start -----" );
+	BOOST_MESSAGE( "----- set_non_blocking_mode test start -----" );
 	
 	test_mirror_server test_server;
 	
@@ -297,8 +321,8 @@ void set_non_blocking_mode_test(){
 		sleep(1);
 	}
 
-	// ## set non blocking mode test [1] set non blocking mode success error_code object
-	std::cout << "set non blocking mode test [1] set non blocking mode success error_code object" << std::endl;
+	// unit_test [1] set_non_blocking_mode test set non blocking mode success error_code object
+	std::cout << "[1] set_non_blocking_mode test set non blocking mode success error_code object" << std::endl;
 	test_obj.set_non_blocking_mode(ec);
 	BOOST_CHECK(!ec);
 	
@@ -308,17 +332,17 @@ void set_non_blocking_mode_test(){
 	test_server.bstop_flag = true;
 	server_thread.join();
 	
-	// ## set non blocking mode test [2] set non blocking mode faile error_code object
-	std::cout << "set non blocking mode test [2] set non blocking mode faile error_code object" << std::endl;
+	// unit_test [2] set_non_blocking_mode test set non blocking mode faile error_code object
+	std::cout << "[2] set_non_blocking_mode test set non blocking mode faile error_code object" << std::endl;
 	test_obj.set_non_blocking_mode(ec);
 	BOOST_CHECK(ec);
 	
-	BOOST_MESSAGE( "----- set non blocking mode test end -----" );
+	BOOST_MESSAGE( "----- set_non_blocking_mode test end -----" );
 }
 
 
 void write_some_read_some_test(){
-	BOOST_MESSAGE( "----- write some read some test start -----" );
+	BOOST_MESSAGE( "----- write_some read_some test start -----" );
 	
 	test_mirror_server test_server;
 	
@@ -366,6 +390,7 @@ void write_some_read_some_test(){
 	// ## write some read some test [1] size 0 
 	send_size = 0;
 	test_server.brecv_triger = true;
+	test_server.data_size = send_size;
 	while(true){
 		res_size = test_obj.write_some(boost::asio::buffer(send_buff, send_size),ec);
 		if(ec){
@@ -375,27 +400,29 @@ void write_some_read_some_test(){
 		}
 		break;
 	}
-	// ## write some read some test [1] size 0 write error_code object
-	std::cout << "write some read some test [1] size 0 write error_code object" << std::endl;
+	// unit_test [1] write_some & read_some test size 0 write error_code object
+	std::cout << "[1] write_some & read_some test size 0 write error_code object" << std::endl;
 	BOOST_CHECK(!ec);
-	// ## write some read some test [2] size 0 write size
-	std::cout << "write some read some test [2] size 0 write send size" << std::endl;
+
+	// unit_test [2] write_some & read_some test size 0 write size
+	std::cout << "[2] write_some & read_some test size 0 write size" << std::endl;
 	BOOST_CHECK_EQUAL(res_size, send_size);
 	
 	sleep(1);
 	
 	res_size = test_obj.read_some(boost::asio::buffer(recv_buff, MAX_BUFFER_SIZE),ec);
 	
-	// ## write some read some test [3] size 0 read error_code object
-	std::cout << "write some read some test [3] size 0 read error_code object" << std::endl;
+	// unit_test [3] write_some & read_some test size 0 read error_code object
+	std::cout << "[3] write_some & read_some test size 0 read error_code object" << std::endl;
 	BOOST_CHECK_EQUAL(ec,boost::asio::error::try_again);
 	
-	// ## write some read some test [4] size 0 read size
-	std::cout << "write some read some test [4] size 0 read size" << std::endl;
+	// unit_test [4] write_some & read_some test size 0 read size
+	std::cout << "[4] write_some & read_some test size 0 read size" << std::endl;
 	BOOST_CHECK_EQUAL(res_size,0UL);
 		
 	// size 1	
 	send_size = 1;
+	test_server.data_size = send_size;
 	send_buff[0] = 'A';
 	recv_buff[0] = 'B';
 	
@@ -409,11 +436,13 @@ void write_some_read_some_test(){
 		}
 		break;
 	}
-	// ## write some read some test [5] size 1 write error_code object
-	std::cout << "write some read some test [5] size 1 write error_code object" << std::endl;
+
+	// unit_test [5] write_some & read_some test size 1 write error_code object
+	std::cout << "[5] write_some & read_some test size 1 write error_code object" << std::endl;
 	BOOST_CHECK(!ec);
-	// ## write some read some test [6] size 1 write send size
-	std::cout << "write some read some test [6] size 1 write size" << std::endl;
+
+	// unit_test [6] write_some & read_some test size 1 write send size
+	std::cout << "[6] write_some & read_some test size 1 write send size" << std::endl;
 	BOOST_CHECK_EQUAL(res_size, send_size);
 
 	while(true){
@@ -425,20 +454,23 @@ void write_some_read_some_test(){
 		}
 		break;
 	}
-	// ## write some read some test [7] size 1 read error_code object
-	std::cout << "write some read some test [7] size 1 read error_code object" << std::endl;
+
+	// unit_test [7] write_some & read_some test size 1 read error_code object
+	std::cout << "[7] write_some & read_some test size 1 read error_code object" << std::endl;
 	BOOST_CHECK(!ec);
-	// ## write some read some test [8] size 1 read size
-	std::cout << "write some read some test [8] size 1 read size" << std::endl;
+
+	// unit_test [8] write_some & read_some test size 1 read size
+	std::cout << "[8] write_some & read_some test size 1 read size" << std::endl;
 	BOOST_CHECK_EQUAL(res_size, send_size);
 	
-	// ## write some read some test [9] size 1 data check
-	std::cout << "write some read some test [9] size 1 data check" << std::endl;
+	// unit_test [9] write_some & read_some test size 1 data check
+	std::cout << "[9] write_some & read_some test size 1 data check" << std::endl;
 	BOOST_CHECK_EQUAL(send_buff[0],recv_buff[0]);
 	
 		
 	// size MAX_BUFFER_SIZE	
 	send_size = MAX_BUFFER_SIZE;
+	test_server.data_size = send_size;
 	for(int i = 0;i < (int)send_size;i++)
 		send_buff[i] = 'A';
 	
@@ -464,12 +496,13 @@ void write_some_read_some_test(){
 			}
 		}
 	}
-	// ## write some read some test [10] size MAX_BUFFER_SIZE write error_code object
-	std::cout << "write some read some test [10] size MAX_BUFFER_SIZE write and read error_code object" << std::endl;
+
+	// unit_test [10] write_some & read_some test size MAX_BUFFER_SIZE write error_code object
+	std::cout << "[10] write_some & read_some test size MAX_BUFFER_SIZE write error_code object" << std::endl;
 	BOOST_CHECK(!ec);
 	
-	// ## write some read some test [11] size MAX_BUFFER_SIZE write size
-	std::cout << "write some read some test [11] size MAX_BUFFER_SIZE write size" << std::endl;		
+	// unit_test [11] write_some & read_some test size MAX_BUFFER_SIZE write size
+	std::cout << "[11] write_some & read_some test size MAX_BUFFER_SIZE write size" << std::endl;		
 	BOOST_CHECK_EQUAL(send_data_size, send_size);
 
 	
@@ -488,18 +521,20 @@ void write_some_read_some_test(){
 			}
 		}
 	}
-	// ## write some read some test [12] size MAX_BUFFER_SIZE read error_code object
-	std::cout << "write some read some test [12] size MAX_BUFFER_SIZE read error_code object" << std::endl;
+
+	// unit_test [12] write_some & read_some test size MAX_BUFFER_SIZE read error_code object
+	std::cout << "[12] write_some & read_some test size MAX_BUFFER_SIZE read error_code object" << std::endl;
 	BOOST_CHECK(!ec);
 	
-	// ## write some read some test [13] size MAX_BUFFER_SIZE read size
-	std::cout << "write some read some test [13] size MAX_BUFFER_SIZE read size" << std::endl;		
+	// unit_test [13] write_some & read_some test size MAX_BUFFER_SIZE read size
+	std::cout << "[13] write_some & read_some test size MAX_BUFFER_SIZE read size" << std::endl;		
 	BOOST_CHECK_EQUAL(receve_data_size, send_size);
 	
-	// ## write some read some test [14] size MAX_BUFFER_SIZE data check
-	std::cout << "write some read some test [14] size MAX_BUFFER_SIZE data check" << std::endl;
+	// unit_test [14] write_some & read_some test size MAX_BUFFER_SIZE data check
+	std::cout << "[14] write_some & read_some test size MAX_BUFFER_SIZE data check" << std::endl;
 	for(int i = 0;i < (int)send_size;i++){
 		BOOST_CHECK_EQUAL(send_buff[i],recv_buff[i]);
+		if(send_buff[i] != recv_buff[i]) break;
 	}
 	
 	test_obj.test_close(ec);
@@ -508,7 +543,7 @@ void write_some_read_some_test(){
 	test_server.bstop_flag = true;
 	server_thread.join();
 	
-	BOOST_MESSAGE( "----- write some read some test end -----" );
+	BOOST_MESSAGE( "----- write_some & read_some test end -----" );
 }
 
 void close_test(){
@@ -524,6 +559,7 @@ void close_test(){
 		
 	// recv cont
 	test_server.req_recv_cnt = 1;
+	test_server.data_size = 1;
 		
 	// test server start
 	boost::thread server_thread(boost::bind(&test_mirror_server::run,&test_server));
@@ -539,7 +575,7 @@ void close_test(){
 	boost::asio::ip::tcp::endpoint connect_end(boost::asio::ip::address::from_string(DUMMI_SERVER_IP), DUMMI_SERVER_PORT);
 	boost::asio::io_service io;
 	boost::system::error_code ec;
-	test_socket_class test_obj(io);	
+	test_socket_class test_obj(io);
 	test_obj.connect(connect_end,ec);
 	BOOST_CHECK(!ec);
 	
@@ -551,20 +587,20 @@ void close_test(){
 	
 	BOOST_CHECK(!test_server.bdisconnect_flag);
 	
-	// ## close test [1] close success error_code object
-	std::cout << "close test [1] close success error_code object" << std::endl;
+	// unit_test [1] close test close success error_code object
+	std::cout << "[1] close test close success error_code object" << std::endl;
 	test_obj.close(ec);
 	BOOST_CHECK(!ec);
 	
 	test_server.brecv_triger = true;
 	sleep(1);
 	
-	// ## close test [2] open_flag set false
-	std::cout << "close test [2] open_flag set false" << std::endl;
+	// unit_test [2] close test open_flag set false
+	std::cout << "[2] close test open_flag set false" << std::endl;
 	BOOST_CHECK(!test_obj.get_open_flag());
 	
-	// ## close test [3] disconnect check
-	std::cout << "close test [3] disconnect check" << std::endl;
+	// unit_test [3] close test disconnect check
+	std::cout << "[3] close test disconnect check" << std::endl;
 	BOOST_CHECK(test_server.bdisconnect_flag);
 	
 	test_server.breq_close_wait_flag = false;	
@@ -574,20 +610,20 @@ void close_test(){
 }
 
 void get_socket_test(){
-	BOOST_MESSAGE( "----- get socket test start -----" );
+	BOOST_MESSAGE( "----- get_socket test start -----" );
 	
 	boost::asio::io_service io;
 	test_socket_class test_obj(io);
 	
-	// ## get socket test [1] socket check
-	std::cout << "get socket test [1] socket check" << std::endl;
+	// unit_test [1] get_socket socket check
+	std::cout << "[1] get_socket socket check" << std::endl;
 	
 	boost::asio::ip::tcp::socket& chk_socket = test_obj.get_socket();
 	boost::asio::ip::tcp::socket* pref_pointer = test_obj.get_socket_pointer();
 	
 	BOOST_CHECK_EQUAL(&chk_socket,pref_pointer);
 	
-	BOOST_MESSAGE( "----- get socket test end -----" );	
+	BOOST_MESSAGE( "----- get_socket test end -----" );	
 }
 
 
@@ -658,22 +694,22 @@ void connect_lock_test(){
 	test_obj.test_thread_wait.unlock();
 	sleep(1);
 	
-	// ## connect lock test [1] thread block test (mutex lock)
-	std::cout << "connect lock test [1] thread block test (mutex lock)" << std::endl;
+	// unit_test [1] connect lock test thread block test (mutex lock)
+	std::cout << "[1] connect lock test thread block test (mutex lock)" << std::endl;
 	BOOST_CHECK(test_obj.befor_thread_id == test_id);
 	BOOST_CHECK(test_obj.after_thread_id == proc_id);
 	
 	test_obj.mutex_unlock();
 	sleep(1);
 	
-	// ## connect lock test [2] thread run test (mutex unlock)
-	std::cout << "connect lock test [2] thread run test (mutex unlock)" << std::endl;
+	// unit_test [2] connect lock test thread run test (mutex unlock)
+	std::cout << "[2] connect lock test thread run test (mutex unlock)" << std::endl;
 	BOOST_CHECK(test_obj.befor_thread_id == test_id);
 	BOOST_CHECK(test_obj.after_thread_id == test_id);
 	
 	
-	// ## connect lock test [3] thread run after mutex unlock test
-	std::cout << "connect lock test [3] thread run after mutex unlock test" << std::endl;
+	// unit_test [3] connect lock test thread run after mutex unlock test
+	std::cout << "[3] connect lock test thread run after mutex unlock test" << std::endl;
 	BOOST_CHECK(test_obj.mutex_trylock());
 	test_obj.mutex_unlock();
 	
@@ -746,22 +782,22 @@ void close_lock_test(){
 	test_obj.test_thread_wait.unlock();
 	sleep(1);
 	
-	// ## close lock test [1] thread block test (mutex lock)
-	std::cout << "close lock test [1] thread block test (mutex lock)" << std::endl;
+	// unit_test [1] close lock test thread block test (mutex lock)
+	std::cout << "[1] close lock test thread block test (mutex lock)" << std::endl;
 	BOOST_CHECK(test_obj.befor_thread_id == test_id);
 	BOOST_CHECK(test_obj.after_thread_id == proc_id);
 	
 	test_obj.mutex_unlock();
 	sleep(1);
 	
-	// ## close lock test [2] thread run test (mutex unlock)
-	std::cout << "close lock test [2] thread run test (mutex unlock)" << std::endl;
+	// unit_test [2] close lock test thread run test (mutex unlock)
+	std::cout << "[2] close lock test  thread run test (mutex unlock)" << std::endl;
 	BOOST_CHECK(test_obj.befor_thread_id == test_id);
 	BOOST_CHECK(test_obj.after_thread_id == test_id);
 	
 	
-	// ## close lock test [3] thread run after mutex unlock test
-	std::cout << "close lock test [3] thread run after mutex unlock test" << std::endl;
+	// unit_test [3] close lock test thread run after mutex unlock test
+	std::cout << "[3] close lock test thread run after mutex unlock test" << std::endl;
 	BOOST_CHECK(test_obj.mutex_trylock());
 	test_obj.mutex_unlock();
 	
