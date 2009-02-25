f9f6125380263dc0553d6fc9f4bcb100c7153cf4
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-02-25 02:31:12 +0000

    #312
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-IT@7251 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/session_test/tcp_socket/tcp_socket_test.cpp
+++ unit_tests/session_test/tcp_socket/tcp_socket_test.cpp
@@ -805,6 +805,73 @@ void close_lock_test(){
 	
 }
 
+void is_open_test(){
+	
+	BOOST_MESSAGE( "----- is_open test start -----" );
+	
+	test_mirror_server test_server;
+	
+	// accept req
+	test_server.breq_acc_flag = true;
+	// close wait req
+	test_server.breq_close_wait_flag = true;
+		
+	// recv cont
+	test_server.req_recv_cnt = 1;
+	test_server.data_size = 1;
+		
+	// test server start
+	boost::thread server_thread(boost::bind(&test_mirror_server::run,&test_server));
+	
+	while( !test_server.brun_flag ){
+		sleep(1);
+	}
+	
+	std::cout << "ready dummy mirror server" << std::endl;
+	
+	
+	
+	boost::asio::ip::tcp::endpoint connect_end(boost::asio::ip::address::from_string(DUMMI_SERVER_IP), DUMMI_SERVER_PORT);
+	boost::asio::io_service io;
+	boost::system::error_code ec;
+	test_socket_class test_obj(io);
+
+	// unit_test [1] is_open before connect check
+	std::cout << "[1] is_open before connect check" << std::endl;
+	BOOST_CHECK(!test_obj.is_open());
+
+	test_obj.connect(connect_end,ec);
+	BOOST_CHECK(!ec);
+
+	// unit_test [2] is_open after connect check
+	std::cout << "[2] is_open after connect check" << std::endl;
+	BOOST_CHECK(test_obj.is_open());
+	
+	while(!test_server.bconnect_flag){
+		sleep(1);
+	}
+	
+	BOOST_CHECK(test_obj.get_open_flag());
+	
+	BOOST_CHECK(!test_server.bdisconnect_flag);
+	
+	test_obj.close(ec);
+	BOOST_CHECK(!ec);
+
+	// unit_test [3] is_open close after check
+	std::cout << "[3] is_open close after check" << std::endl;
+	BOOST_CHECK(!test_obj.is_open());
+	
+	test_server.brecv_triger = true;
+	sleep(1);
+	
+	test_server.breq_close_wait_flag = false;	
+	test_server.bstop_flag = true;
+	server_thread.join();	
+	BOOST_MESSAGE( "----- is_open test end -----" );	
+}
+
+
 test_suite*	init_unit_test_suite( int argc, char* argv[] ){
 
 	test_suite* ts = BOOST_TEST_SUITE( "l7vs::tcp_socket class test" );
@@ -817,6 +884,7 @@ test_suite*	init_unit_test_suite( int argc, char* argv[] ){
 	ts->add( BOOST_TEST_CASE( &write_some_read_some_test ) );
 	ts->add( BOOST_TEST_CASE( &close_test ) );
 	ts->add( BOOST_TEST_CASE( &close_lock_test ) );
+	ts->add( BOOST_TEST_CASE( &is_open_test ) );
 
 	framework::master_test_suite().add( ts );
 
