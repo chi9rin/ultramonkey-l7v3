47b49a6a2bcea54ce6774007df4e533d20fff988
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-03-25 02:49:07 +0000

    #336 utility.hにコメント追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-PT@7749 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/utility.h
+++ include/utility.h
@@ -1,6 +1,20 @@
+//
+//	utility macro header.
+//	(C) copyright n.nakai@sdy.co.jp
+//	Distributed under the Boost Software License, Version 1.0. (See accompanying
+//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
+//
+//
 #ifndef  __UTILITY_H__
 #define  __UTILITY_H__
 
+
+//! gcc extention code output macro.
+//! static "if" weight algolithm teach compiler.
+//! compiler output CPU TBL code.
+//! usage:
+//!	if( likely( hoge ) ){ fasecall } else { slowcall }
+//! if( unlikely( hoge ) ){ slowcall } else { fastcall }
 #define likely(c)   __builtin_expect(!!(c), 1)
 #define unlikely(c) __builtin_expect(!!(c), 0)
 
