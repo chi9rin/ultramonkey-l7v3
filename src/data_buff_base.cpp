cd925e68f68768473c92d97fd78443991dd989bd
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-03-30 09:14:21 +0000

    #337
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-PT@7829 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- src/data_buff_base.cpp
+++ src/data_buff_base.cpp
@@ -1,13 +1,28 @@
-//
-//!	@file	data_buff_base.cpp
-//!	@brief	session object receive and send buffer class
-//
-//	copyright (c) 2008 TOKYO COMPUTER SERVICE CO.,LTD.
-//	mail: 
-//
-//	Distributed under the Boost Software License, Version 1.0.(See accompanying
-//	file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt
-//
+/*!
+ *	@file	data_buff_base.cpp
+ *	@brief	session object receive and send buffer class
+ *
+ * L7VSD: Linux Virtual Server for Layer7 Load Balancing
+ * Copyright (C) 2009  NTT COMWARE Corporation.
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU Lesser General Public
+ * License as published by the Free Software Foundation; either
+ * version 2.1 of the License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public
+ * License along with this library; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301 USA
+ *
+ **********************************************************************/
+
+
 
 #include <boost/thread/thread.hpp>
 
