994cf6a897f0b7692904f6fae2256f805f0c67d4
kanda <kanda@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-04 01:33:22 +0000

    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7882 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/atomic.h
+++ include/atomic.h
@@ -1,48 +1,43 @@
 #ifndef ATOMIC_H
 #define ATOMIC_H
 #include <boost/utility.hpp>
-#include <iostream>
 
 namespace l7vs{
 
 template<class T> class atomic :boost::noncopyable{
 protected:
-	T volatile *_p;
+	T volatile _p;
 public:
-	atomic(){
-		_p = new T;
-	}
-
-	T get(){ return *_p; }
+	T get(){ return _p; }
 	atomic& operator=(const T& _q) {
-		__sync_lock_test_and_set(_p,_q);
+		__sync_lock_test_and_set(&_p,_q);
 		return *this;
 	}
 	atomic& operator++(){
-		*_p++;
+		_p++;
 		return *this;
 	}
 	atomic& operator--(){
-		*_p--;
+		_p--;
 		return *this;
 	}
-	bool operator<(T* _q) const{
-		return (*_p < *_q);
+	bool operator<(T _q) const{
+		return (_p < _q);
 	}
-	bool operator<=(T* _q) const{
-		return (*_p <= *_q);
+	bool operator<=(T _q) const{
+		return (_p <= _q);
 	}
-	bool operator>(T* _q) const{
-		return (*_p > *_q);
+	bool operator>(T _q) const{
+		return (_p > _q);
 	}
-	bool operator>=(T* _q) const{
-		return (*_p >= *_q);
+	bool operator>=(T _q) const{
+		return (_p >= _q);
 	}
-	bool operator==(T* _q) const{
-		return (*_p == *_q);
+	bool operator==(T _q) const{
+		return (_p == _q);
 	}
-	bool operator!=(T* _q) const{
-		return (*_p != *_q);
+	bool operator!=(T _q) const{
+		return (_p != _q);
 	}
 };
 
@@ -51,45 +46,40 @@ public:
 template <>
 class atomic<int>{
 protected:
-	int volatile *_p;
+	int volatile _p;
 public:
-	//constractor
-	atomic(){
-		_p = new int;
-	}
-
 	int get(){
-		return *_p;
+		return _p;
 	}
 	atomic& operator=(const int _q) {
-		__sync_lock_test_and_set(_p,_q);
+		__sync_lock_test_and_set(&_p,_q);
 		return *this;
 	}
 	int operator++(int){
-		__sync_add_and_fetch(_p,1);
-		return *_p;
+		__sync_add_and_fetch(&_p,1);
+		return _p;
 	}
 	int operator--(int){
-		__sync_sub_and_fetch(_p,1);
-		return *_p;
+		__sync_sub_and_fetch(&_p,1);
+		return _p;
 	}
 	bool operator<(const int _q) const{
-		return (*_p < _q);
+		return (_p < _q);
 	}
 	bool operator<=(const int _q) const{
-		return (*_p <= _q);
+		return (_p <= _q);
 	}
 	bool operator>(const int _q) const{
-		return (*_p > _q);
+		return (_p > _q);
 	}
 	bool operator>=(const int _q) const{
-		return (*_p >= _q);
+		return (_p >= _q);
 	}
 	bool operator==(const int _q) const{
-		return (*_p == _q);
+		return (_p == _q);
 	}
 	bool operator!=(const int _q) const{
-		return (*_p != _q);
+		return (_p != _q);
 	}
 
 };
@@ -98,47 +88,41 @@ public:
 template <>
 class atomic<unsigned long long>{
 protected:
-	unsigned long long volatile *_p;
+	unsigned long long volatile _p;
 public:
-	//constractor
-	atomic(){
-		_p = new unsigned long long;
-	}
-
 	unsigned long long get(){
-		return *_p;
+		return _p;
 	}
 	atomic& operator=(const unsigned long long _q) {
-		__sync_lock_test_and_set(_p,_q);
+		__sync_lock_test_and_set(&_p,_q);
 		return *this;
 	}
 	unsigned long long operator++(int){
-		__sync_add_and_fetch(_p,1);
-		return *_p;
+		__sync_add_and_fetch(&_p,1);
+		return _p;
 	}
 	unsigned long long operator--(int){
-		__sync_sub_and_fetch(_p,1);
-		return *_p;
+		__sync_sub_and_fetch(&_p,1);
+		return _p;
 	}
 	bool operator<(const unsigned long long _q) const{
-		return (*_p < _q);
+		return (_p < _q);
 	}
 	bool operator<=(const unsigned long long _q) const{
-		return (*_p <= _q);
+		return (_p <= _q);
 	}
 	bool operator>(const unsigned long long _q) const{
-		return (*_p > _q);
+		return (_p > _q);
 	}
 	bool operator>=(const unsigned long long _q) const{
-		return (*_p >= _q);
+		return (_p >= _q);
 	}
 	bool operator==(const unsigned long long _q) const{
-		return (*_p == _q);
+		return (_p == _q);
 	}
 	bool operator!=(const unsigned long long _q) const{
-		return (*_p != _q);
+		return (_p != _q);
 	}
-
 };
 
 } //namespace l7vsd
