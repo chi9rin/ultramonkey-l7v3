6e80963f1fd3ecbd9f1d8cf0149afe74ebd6e190
nakai <nakai@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-08 09:36:21 +0000

    atomicテスト用コード追加
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7890 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ unit_tests/bentch/atomic_test.cpp
@@ -0,0 +1,82 @@
+#include <pthread.h>
+#include <iostream>
+#include <boost/lexical_cast.hpp>
+#include <vector>
+#include "../../include/atomic.h"
+#include "rdtsc64.h"
+
+
+typedef	l7vs::atomic< unsigned long long >	atomic_ulong_t;
+atomic_ulong_t	ulong_value;
+
+pthread_mutex_t	 mutex;
+unsigned long long lock_ulong_value;
+
+void*	thread_func_atomic( void* param ){
+	unsigned long long starttime, endtime;
+	RDTSC64( starttime );
+	for( int i = 0 ; i < 10000; ++i ){
+		ulong_value -= 1000;
+		ulong_value -= 1000;
+	}
+	RDTSC64( endtime );
+	std::cout << endtime - starttime << std::endl;
+	return 0;
+}
+
+void*	thread_func_mutex( void* param ){
+	unsigned long long starttime, endtime;
+	RDTSC64( starttime );
+	for( int i = 0 ; i < 10000; ++i ){
+		pthread_mutex_lock( &mutex );
+		lock_ulong_value += 1000;
+		lock_ulong_value -= 1000;
+		pthread_mutex_unlock( &mutex );
+	}
+	RDTSC64( endtime );
+	std::cout << endtime - starttime << std::endl;
+	return 0;
+}
+
+int main( int argc, char* argv[] ){
+	ulong_value = 0;
+	lock_ulong_value = 0;
+	pthread_mutex_init( &mutex, NULL );
+	if( argc < 2 ){
+		std::cout << "usage : a.out [thread_num] " << std::endl;
+		return 0;
+	}
+
+	int count = boost::lexical_cast<int>( argv[1] );
+	std::vector<pthread_t>	thread_vec;
+	pthread_t	thd;
+
+	//atomic version
+	std::cout << "atomic func time start" << std::endl;
+	for( int i = 0; i < count; ++i ){
+		pthread_create( &thd, NULL, thread_func_atomic, NULL );
+		thread_vec.push_back( thd );
+	}
+	for( std::vector<pthread_t>::iterator itr = thread_vec.begin();
+	     itr != thread_vec.end();
+	     ++itr ){
+		pthread_join( *itr, NULL );
+	}
+	thread_vec.clear();
+
+	//mutex version
+	std::cout << "mutex func time start" << std::endl;
+	for( int i = 0; i < count; ++i ){
+		pthread_create( &thd, NULL, thread_func_mutex, NULL );
+		thread_vec.push_back( thd );
+	}
+	for( std::vector<pthread_t>::iterator itr = thread_vec.begin();
+	     itr != thread_vec.end();
+	     ++itr ){
+		pthread_join( *itr, NULL );
+	}
+
+	pthread_mutex_destroy( &mutex );
+
+	return 0;
+}
---
+++ unit_tests/bentch/rdtsc64.h
@@ -0,0 +1,18 @@
+#ifndef	RDTSC64_H
+#define	RDTSC64_H
+//
+//	this code is AMD64 only. don't use other platform.
+//	n.nakai@sdy.co.jp
+//
+//
+
+#define	RDTSC64(X) __asm__ __volatile__ (	\
+		"rdtsc;" 		\
+		"movq %%rdx,%0;" 	\
+		"salq $32,%0;" 		\
+		"orq %%rax, %0" 	\
+		: "=r"(X) 		\
+		: 			\
+		: "%rax", "%rdx" );
+
+#endif	//RDTSC64_H
