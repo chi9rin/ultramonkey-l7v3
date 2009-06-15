f9c557676dc6a0ebd046108ce0476a334ce49663
kanda <kanda@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-15 04:29:11 +0000

    Modify lockfree_hashmap.h
    lockfree_hashmapの修正
    IFの修正
    void	insert( const Tkey* key, const Tvalue* value )
    Tvalue*	find( const Tkey* key )
    void	erase( const Tkey*	key )
    
    
    
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7916 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/lockfree_hashmap.h
+++ include/lockfree_hashmap.h
@@ -3,12 +3,9 @@
 
 #include <boost/functional/hash.hpp>
 #include <boost/noncopyable.hpp>
-#include <boost/foreach.hpp>
-#include <boost/shared_ptr.hpp>
 #include <boost/function.hpp>
 
 namespace l7vs{
-
 
 //Argument Tkey, its size must be 64bit or less.
 template < class Tkey, class Tvalue, class Hash = boost::hash< Tkey > >
@@ -17,10 +14,10 @@ protected:
 
 	volatile mutable size_t	element_num;
 	struct container{
-		Tkey		key;
+		Tkey*		key;
 		Tvalue*		value;
 		bool		rehash;
-		explicit container() : rehash( false ) {}
+		explicit container() : key ( NULL ), value( NULL ) , rehash( false ) {}
 	};
 
 	volatile mutable size_t	all_ctr;
@@ -43,52 +40,21 @@ public:
 		delete [] hashmap;
 	}
 
-
-	//bucket finder
-	bool	find_bucket( size_t& hash_value, const Tkey key, Tkey*& pre_key, bool insert ){
-		for(;;){
-			if( hashmap[hash_value].key == NULL ){
-				if( insert ) return true;
-				if( hashmap[hash_value].rehash )
-					hash_value = re_hashvalue( hash_value );
-				else
-					return( false );
-			}
-			else if( hashmap[hash_value].key == key ) {
-				*pre_key = hashmap[hash_value].key;
-				return true;
-			}
-			else{
-				if( insert ){
-					hashmap[hash_value].rehash = true;
-					hash_value = re_hashvalue( hash_value );
-				}
-				else{
-					if( !hashmap[hash_value].rehash ) return false;
-					hash_value = re_hashvalue( hash_value );
-				}
-			}
-		}
-	}
-
 	//inserter
-	void	insert( const Tkey key, const Tvalue* value ){
-		size_t	hashvalue = get_hashvalue( key );
+	void	insert( const Tkey* key, const Tvalue* value ){
+		size_t	hashvalue = get_hashvalue( *key );
 		Tkey*	pre_key = NULL;
-
 		do{
 			if( !find_bucket( hashvalue, key, pre_key, true ) ) return;
 		}
-		while( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, *pre_key, key ) );
-
-		hashmap[hashvalue].key		= const_cast< Tkey >( key );
+		while( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, pre_key, key ) );
 		hashmap[hashvalue].value	= const_cast< Tvalue* >( value );
 		__sync_add_and_fetch( &all_ctr, 1 );
 	}
 
 	//finder
-	Tvalue*	find( const Tkey key ){
-		size_t	hashvalue = get_hashvalue( key );
+	Tvalue*	find( const Tkey* key ){
+		size_t	hashvalue = get_hashvalue( *key );
 		Tkey*	pre_key = NULL;
 		if( find_bucket( hashvalue, key, pre_key, false ) )
 			return hashmap[hashvalue].value;
@@ -97,29 +63,27 @@ public:
 	}
 
 	//eracer
-	void	erase( const Tkey	key ){
-		size_t	hashvalue = get_hashvalue( key );
+	void	erase( const Tkey*	key ){
+		size_t	hashvalue = get_hashvalue( *key );
 		Tkey*	pre_key = NULL;
-
 		do{
 			if( !find_bucket( hashvalue, key, pre_key, false ) ) return;
 		}
-		while( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, *pre_key, NULL ) );
-
-		hashmap[hashvalue].key		= NULL;
+		while( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, pre_key, NULL ) );
 		hashmap[hashvalue].value	= NULL;
 		__sync_sub_and_fetch( &all_ctr, 1 );
 	}
 
 	//poper
-	void	pop( Tkey* key, Tvalue* value ){
+	void	pop( Tkey*& key, Tvalue*& value ){
 		for( size_t i = 0 ; i < element_num; ++i ){
 			if( hashmap[i].key ){
-				*key	= hashmap[i].key;
+				key	= hashmap[i].key;
 				value	= hashmap[i].value;
 				hashmap[i].key		= NULL;
 				hashmap[i].value	= NULL;
 				__sync_sub_and_fetch( &all_ctr, 1 );
+				return;
 			}
 		}
 	}
@@ -145,17 +109,41 @@ public:
 			}
 		}
 	}
-protected:
-	size_t	get_hashvalue( Tkey key ){
-		return hasher()( key ) % element_num;
-	}
 
+private:
+	//bucket finder
+	bool	find_bucket( size_t& hash_value, const Tkey*& key, Tkey*& pre_key, bool insert ){
+		for(;;){
+			if( hashmap[hash_value].key == NULL ){
+				if( insert ) return true;
+				if( hashmap[hash_value].rehash )
+					hash_value = re_hashvalue( hash_value );
+				else
+					return( false );
+			}
+			else if( hashmap[hash_value].key == key ) {
+				pre_key = hashmap[hash_value].key;
+				return true;
+			}
+			else{
+				if( insert ){
+					hashmap[hash_value].rehash = true;
+					hash_value = re_hashvalue( hash_value );
+				}
+				else{
+					if( !hashmap[hash_value].rehash ) return false;
+					hash_value = re_hashvalue( hash_value );
+				}
+			}
+		}
+	}
+	size_t	get_hashvalue(const Tkey& key ){
+		return hasher( key ) % element_num;
+	}
 	size_t	re_hashvalue( size_t& key ){
 		return ((key+1) % element_num);
 	}
 };
-
-
 
 }	//namespace l7vs
 #endif	//LOCKFREE_MAP_H
