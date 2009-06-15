ca2ef7fdc92f0350dda990cb04657edfba1b00e9
okada <okada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-15 07:16:56 +0000

    virtualservice_tcpのテストコードを更新
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7921 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- unit_tests/virtualservice_test/vs_tcp_test.cpp
+++ unit_tests/virtualservice_test/vs_tcp_test.cpp
@@ -1314,10 +1314,10 @@ test_suite*	init_unit_test_suite( int argc, char* argv[] ){
 	test_suite* ts = BOOST_TEST_SUITE( "virtualservice_base_test" );
 
 	// add test case to test suite
-//	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test1 ) );
-//	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test2 ) );
+	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test1 ) );
+	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test2 ) );
 	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test3 ) );
-//	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test4 ) );
+	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test4 ) );
 	ts->add( BOOST_TEST_CASE( &virtualservice_tcp_test5 ) );
 
 	framework::master_test_suite().add( ts );
