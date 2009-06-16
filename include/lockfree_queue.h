6fdf0b29b61ab714f4029931b259ddeab4a1eb13
kanda <kanda@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-16 05:31:21 +0000

    lockfree_queue.h修正 SEGV対応 （有限配列ver）
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7931 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/lockfree_queue.h
+++ include/lockfree_queue.h
@@ -2,43 +2,91 @@
 #define LOCKFREE_QUEUE_H
 
 #include <boost/noncopyable.hpp>
-#include <queue>
-#include "wrlock.h"
 
 namespace l7vs{
 
 template<class Tvalue>
 class lockfree_queue : protected boost::noncopyable {
 protected:
-	std::queue< Tvalue* >	stl_queue;
-	wr_mutex		mutex;
+	struct node_type {
+		Tvalue*			value;
+		node_type() : value( NULL ) {}
+	};
+	mutable volatile size_t	counter;
+	volatile size_t			headloc;
+	volatile size_t			tailloc;
+
+	volatile node_type*		node;
+	size_t					element_num;
 public:
-	explicit lockfree_queue(){ 
+	// constractor
+	explicit lockfree_queue(size_t num = 65535) : counter(0),element_num( num ){
+		node = new node_type[element_num];
+		headloc = tailloc = 0;
 	}
 
 	// destractor
 	~lockfree_queue(){
+		delete [] node;
 	}
-	
-	void push(const Tvalue* value){
-		mutex.wrlock();
-		stl_queue.push( const_cast< Tvalue* >( value ) );
-		mutex.unlock();
+
+	bool push(const Tvalue* value){
+		size_t tail,nexttail;
+		//transaction st
+		while(true){
+			tail = tailloc;
+			nexttail = get_num_next(tail);
+			if ( node[tail].value) {
+				return false;
+			}
+			if ( __sync_bool_compare_and_swap(&tailloc,tail,nexttail) ) break;
+		}
+		//transaction ed
+		node[tail].value = const_cast<Tvalue*>( value );
+		__sync_add_and_fetch( &counter, 1 );
+		return true;
 	}
 
 	Tvalue* pop() {
-		mutex.wrlock();
-		Tvalue*	ptr = stl_queue.front();
-		stl_queue.pop();
-		mutex.unlock();
-		return ptr;
+		size_t head,nexthead;
+		Tvalue* rtnvalue;
+		//transaction st
+		while(true){
+			head = headloc;
+			nexthead = get_num_next(head);
+			if(!(node[head].value)){
+				if(headloc==tailloc){
+					//false
+					return NULL;
+				}
+			}else{
+				if( __sync_bool_compare_and_swap(&headloc,head,nexthead) ) {
+					rtnvalue = node[head].value;
+					break;
+				}
+			}
+		}
+		//transaction ed
+		__sync_sub_and_fetch( &counter, 1 );
+		node[head].value = NULL;
+		return rtnvalue;
 	}
 
 	bool empty() const{
-		return stl_queue.empty();
+		if( counter ) return false;
+		return true;
 	}
 	size_t size() const{
-		return stl_queue.size();
+		return counter;
+	}
+
+private:
+	size_t get_num_next( const size_t num ){
+		if( num >= element_num ){
+			return 0;
+		}else{
+			return num + 1;
+		}
 	}
 };
 
