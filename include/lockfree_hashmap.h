673bfa0d7bbec0375d6691e07b81614b3eb84b25
kanda <kanda@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-06-19 08:07:33 +0000

    lockfree_hashmap.h の修正 パフォーマンス改善
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-shamshel-refine@7949 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- include/lockfree_hashmap.h
+++ include/lockfree_hashmap.h
@@ -29,10 +29,10 @@ public:
 	hasher_type		hasher;
 
 	// constractor
-	lockfree_hashmap( size_t num = 65535, Hash inhasher = boost::hash< Tkey* >() ) : 
+	lockfree_hashmap( size_t num = 65535, Hash inhasher = boost::hash< Tkey* >() ) :
 		element_num( num ) {
 		hashmap = new container[num];
-		hasher = inhasher;	
+		hasher = inhasher;
 		__sync_lock_test_and_set( &all_ctr, 0 );
 	}
 
@@ -45,10 +45,21 @@ public:
 	void	insert( const Tkey* key, const Tvalue* value ){
 		size_t	hashvalue = get_hashvalue( key );
 		Tkey*	pre_key = NULL;
+		//transaction st
 		do{
-			if( likely( !find_bucket( hashvalue, key, pre_key, true ) ) ) return;
+			for(;;){
+				if( likely ( !hashmap[hashvalue].key ) ) break;
+				else if( unlikely( hashmap[hashvalue].key == key ) ){
+					pre_key = hashmap[hashvalue].key;
+					break;
+				}else{
+					hashmap[hashvalue].rehash = true;
+					hashvalue = get_rehashvalue( hashvalue );
+				}
+			}
 		}
 		while( unlikely( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, pre_key, key ) ) );
+		//transaction ed
 		hashmap[hashvalue].value	= const_cast< Tvalue* >( value );
 		__sync_add_and_fetch( &all_ctr, 1 );
 	}
@@ -56,23 +67,43 @@ public:
 	//finder
 	Tvalue*	find( const Tkey* key ){
 		size_t	hashvalue = get_hashvalue( key );
-		Tkey*	pre_key = NULL;
-		if( likely( find_bucket( hashvalue, key, pre_key, false ) ) )
-			return hashmap[hashvalue].value;
-		else
-			return NULL;
+		for(;;){
+			if( unlikely ( !hashmap[hashvalue].key ) ){
+				if( likely( !hashmap[hashvalue].rehash ) ) return NULL;
+				hashvalue = get_rehashvalue( hashvalue );
+			}else if( likely( hashmap[hashvalue].key == key ) ){
+				return hashmap[hashvalue].value;
+			}else{
+				if( likely( !hashmap[hashvalue].rehash ) ) return NULL;
+				hashvalue = get_rehashvalue( hashvalue );
+			}
+		}
 	}
 
 	//eracer
 	void	erase( const Tkey*	key ){
 		size_t	hashvalue = get_hashvalue( key );
-		Tkey*	pre_key = NULL;
-		do{
-			if( unlikely( !find_bucket( hashvalue, key, pre_key, false ) ) ) return;
+		for(;;){
+			if( unlikely ( !hashmap[hashvalue].key ) ){
+				if( likely( !hashmap[hashvalue].rehash ) ) break;
+				hashvalue = get_rehashvalue( hashvalue );
+			}else if( likely( hashmap[hashvalue].key == key ) ){
+				__sync_lock_test_and_set(&hashmap[hashvalue].key,NULL);
+				hashmap[hashvalue].value	= NULL;
+				__sync_sub_and_fetch( &all_ctr, 1 );
+				break;
+			}else{
+				if( likely( !hashmap[hashvalue].rehash ) ) break;
+				hashvalue = get_rehashvalue( hashvalue );
+			}
 		}
-		while( unlikely( !__sync_bool_compare_and_swap( &hashmap[hashvalue].key, pre_key, NULL ) ) );
-		hashmap[hashvalue].value	= NULL;
-		__sync_sub_and_fetch( &all_ctr, 1 );
+	}
+
+	//clear
+	void	clear(){
+		delete [] hashmap;
+		hashmap = new container[element_num];
+		__sync_lock_test_and_set( &all_ctr, 0 );
 	}
 
 	//poper
@@ -87,13 +118,6 @@ public:
 				return;
 			}
 		}
-	}
-
-	//clear
-	void	clear(){
-		delete [] hashmap;
-		hashmap = new container[element_num];
-		__sync_lock_test_and_set( &all_ctr, 0 );
 	}
 
 	// size
@@ -112,38 +136,12 @@ public:
 	}
 
 private:
-	//bucket finder
-	bool	find_bucket( size_t& hash_value, const Tkey*& key, Tkey*& pre_key, bool insert ){
-		for(;;){
-			if( unlikely (hashmap[hash_value].key == NULL ) ){
-				if( insert ) return true;
-				if( unlikely( hashmap[hash_value].rehash ) )
-					hash_value = re_hashvalue( hash_value );
-				else
-					return( false );
-			}
-			else if( likely( hashmap[hash_value].key == key ) ){
-				pre_key = hashmap[hash_value].key;
-				return true;
-			}
-			else{
-				if( unlikely( insert ) ){
-					hashmap[hash_value].rehash = true;
-					hash_value = re_hashvalue( hash_value );
-				}
-				else{
-					if( unlikely( !hashmap[hash_value].rehash ) ) return false;
-					hash_value = re_hashvalue( hash_value );
-				}
-			}
-		}
-	}
 	//get array number by hasher
 	size_t	get_hashvalue(const Tkey* key ){
 		return hasher( const_cast<Tkey*>(key) ) % element_num;
 	}
 	//reget array number case of collision(method chain)
-	size_t	re_hashvalue( size_t& key ){
+	size_t	get_rehashvalue( size_t& key ){
 		return ((key+1) % element_num);
 	}
 };
