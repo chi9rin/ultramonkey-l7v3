09357df8d49dd6bebe300dd5962dabc65d8a673a
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-12-13 02:48:03 +0000

    テストコード修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-ramiel@9062 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- l7vsd/unit_tests/session_test/tcp_session/tcp_session_test.cpp
+++ l7vsd/unit_tests/session_test/tcp_session/tcp_session_test.cpp
@@ -12796,8 +12796,9 @@ void initialize_ssl_mode_test(){
     bool set_ssl_cache_flag2(false);
     initialize_ssl_mode_test_class test_obj2(vs,io,set_option,listen_endpoint,set_mode,set_context2,set_ssl_cache_flag2,set_ssl_handshake_time_out,plogger);
 
-    SSL* pRet = test_obj2.get_client_ssl_socket().impl()->ssl;
-    test_obj2.get_client_ssl_socket().impl()->ssl = NULL;
+    SSL_METHOD* pRet = test_obj2.get_client_ssl_socket().impl()->ssl->method;
+    test_obj2.get_client_ssl_socket().impl()->ssl->method = NULL;
+
 
     // test call
     msg = test_obj2.initialize();
@@ -12806,7 +12807,7 @@ void initialize_ssl_mode_test(){
     std::cout << "[3] initialize SSL_clear Error case" << std::endl;
     BOOST_CHECK(msg.flag == true);
 
-    test_obj2.get_client_ssl_socket().impl()->ssl = pRet;
+    test_obj2.get_client_ssl_socket().impl()->ssl->method = pRet;
     
 
 
