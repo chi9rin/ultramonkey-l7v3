9a52f52e26c67ff3286c9a4b08b05478d8984fae
yamada <yamada@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2009-12-16 10:46:51 +0000

    テストコード修正
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-ramiel@9185 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- l7vsd/unit_tests/l7vs_logger/logger_ut_test.cpp
+++ l7vsd/unit_tests/l7vs_logger/logger_ut_test.cpp
@@ -8,6 +8,103 @@
 #include <boost/test/unit_test.hpp>
 
 using namespace boost::unit_test_framework;
+
+//using namespace l7vs;
+
+extern void* _start;    //開始位置
+extern void* _etext;    //終了位置
+
+#define BUF_LEN (4096)
+
+//typedef std::map< LOG_CATEGORY_TAG, LOG_LEVEL_TAG>
+//                category_level_map_type;
+
+
+int resetConfigurationCalled = 0;
+
+int getRootLoggerCalled = 0;
+bool getRootLoggerReturnNull = false;
+
+int patternLayoutCtorCalled = 0;
+void* patternLayoutCreated = 0;
+bool patternLayoutNewFail = false;
+
+int fileAppenderCtorCalled = 0;
+void* fileAppenderCreated = 0;
+bool fileAppenderNewFail = false;
+std::string fileAppenderCtorArgFilename = "";
+//log4cxx::LayoutPtr fileAppenderCtorArgLayout = 0;
+int fileAppenderSetFileCalled = 0;
+std::string fileAppenderSetFileArgFilename = "";
+bool fileAppenderSetFileArgAppend = false;
+bool fileAppenderSetFileArgBufferedIO = false;
+size_t fileAppenderSetFileArgBufferSize = 0;
+void* fileAppenderSetFileArgPool = 0;
+bool fileAppenderSetFileException = false;
+
+int poolCtorCalled = 0;
+void* poolCreated = 0;
+
+int syslogAppenderCtorCalled = 0;
+void* syslogAppenderCreated = 0;
+bool syslogAppenderNewFail = false;
+//log4cxx::LayoutPtr syslogAppenderCtorArgLayout = 0;
+int syslogAppenderCtorArgFacility = 0;
+
+int loggerAddAppenderCalled = 0;
+//log4cxx::AppenderPtr loggerAddAppenderArg[2] = {0, 0};
+
+int loggerForcedLogCalled = 0;
+//log4cxx::LevelPtr loggerForcedLogArgLevel = 0;
+
+std::string loggerForcedLogArgMessage = "";
+char loggerForcedLogArgFile[256];
+int loggerForcedLogArgLine = 0;
+bool loggerForcedLogEnabled = true;
+bool loggerForcedLogException = false;
+
+int loggerGetLoggerCalled = 0;
+std::string loggerGetLoggerArgName = "";
+bool loggerGetLoggerReturnNull = false;
+
+int fixedWindowCtorCalled = 0;
+void* fixedWindowCreated = 0;
+bool fixedWindowNewFail = false;
+
+int sizeBaseCtorCalled = 0;
+void* sizeBaseCreated = 0;
+bool sizeBaseNewFail = false;
+
+int strictPolicyCtorCalled = 0;
+void* strictPolicyCreated = 0;
+bool strictPolicyNewFail = false;
+
+int timeSizePolicyCtorCalled = 0;
+void* timeSizePolicyCreated = 0;
+bool timeSizePolicyNewFail = false;
+
+int rollingAppenderCtorCalled = 0;
+void* rollingAppenderCreated = 0;
+bool rollingAppenderNewFail = false;
+
+int loggerGetLevelCalled = 0;
+int loggerSetLevelCalled = 0;
+
+int snmpChangeLevelCalled = 0;
+int snmpChangeLevelArgCat = 0;
+int snmpChangeLevelArgLevel = 0;
+
+int snmpGetLevelCalled = 0;
+//LOG_CATEGORY_TAG snmpGetLevelArgCat;
+//LOG_LEVEL_TAG snmpGetLevelReturns;
+
+int appenderSkeletonSetLayoutCalled = 0;
+void* appenderSkeletonSetLayoutArgLayout = 0;
+
+int rollingAppenderSkeletonActivateOptionsCalled = 0;
+
+int hierarchySetConfiguredCalled = 0;
+
 //using namespace l7vs;
 
 //enum LOG_ROTATION_TAG{ LOG_ROT_SIZE, LOG_ROT_DATE, LOG_ROT_DATE_SIZE };
