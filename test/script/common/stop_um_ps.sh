c0f33a7006619eef8d78bacba4d367f0df9d9002
6638678 <6638678@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2010-06-28 03:37:40 +0000

    未完成版
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-ramiel-epoll-cond@10127 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ test/script/common/stop_um_ps.sh
@@ -0,0 +1,1 @@
+﻿#!/bin/bashif [ -n "`pgrep l7vsd`" ]then${INIT_L7VSD} stopif [ $? -ne 0 ]thenLOG_FATAL "Can not stop l7vsd."exit 1fifiif [ -n "`pgrep l7directord`" ]then${INIT_L7DIRECTORD} stop > /dev/nullif [ -n "`pgrep l7directord`" ]thenLOG_FATAL "Can not stop l7directord."exit 1fifiLOG "stop_um_ps.sh ............OK"
