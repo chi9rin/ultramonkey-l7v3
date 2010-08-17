b0adf38f818f2b93639d31c40720901ff9f549b7
6638678 <6638678@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2010-08-17 06:57:16 +0000

    add test scripts
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-ramiel-epoll-cond@10213 1ed66053-1c2d-0410-8867-f7571e6e31d3

---
+++ test/script/virtualservice/virtualservice-107.sh
@@ -0,0 +1,26 @@
+#!/bin/bash
+
+# set config file
+. ${SET_DEFAULT_CONF}
+\cp -f materials/virtualservice-107-l7vs.cf ${L7VSD_CONF_DIR}/l7vs.cf
+\cp -f materials/virtualservice-107-sslproxy.target.cf ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf
+
+#Add Service
+$L7VSD
+if [ $? -ne 0 ]
+then
+        echo "Test failed: $L7VSD"
+        exit 1
+fi
+usleep 100000
+
+RET=`$L7VSADM -A -t 127.0.0.1:40001 -m sessionless -z ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf 2>&1`
+EXPECT="add vs error : get ssl parameter failed"
+if [ "${RET}" != "${EXPECT}" ]
+then
+        echo "Test failed: $L7VSADM -A -t 127.0.0.1:40001 -m sessionless -z ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf"
+        exit 1
+fi
+
+exit 0
+
---
+++ test/script/virtualservice/virtualservice-126-2.sh
@@ -0,0 +1,24 @@
+#!/bin/bash
+
+# set config file
+. ${SET_DEFAULT_CONF}
+\cp -f materials/virtualservice-126-2-sslproxy.target.cf ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf
+
+#Add Service
+$L7VSD
+if [ $? -ne 0 ]
+then
+        echo "Test failed: $L7VSD"
+        exit 1
+fi
+usleep 100000
+
+$L7VSADM -A -t 127.0.0.1:40001 -m sessionless -z ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf 2>&1
+if [ $? -ne 0 ]
+then
+        echo "Test failed: $L7VSADM -A -t 127.0.0.1:40001 -m sessionless -z ${L7VSD_CONF_DIR}/sslproxy/sslproxy.target.cf"
+        exit 1
+fi
+
+exit 0
+
