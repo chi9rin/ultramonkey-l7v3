9166efcf9cb050923a6f56cd65e83ef5a36c8a0d
0809216 <0809216@1ed66053-1c2d-0410-8867-f7571e6e31d3>
2010-10-07 02:27:50 +0000

    l7vsadm test script change number
    git-svn-id: http://10.144.169.20/repos/um/branches/l7vsd-3.x-ramiel-epoll-cond@10340 1ed66053-1c2d-0410-8867-f7571e6e31d3

--- test/script/l7vsadm/l7vsadm-7-1-5.sh
+++ test/script/l7vsadm/l7vsadm-7-1-5.sh
@@ -11,28 +11,11 @@ then
 fi
 usleep 100000
 
-RET=`$L7VSADM -V | grep "l7vsd_virtualservice"`
-EXPECT="l7vsd_virtualservice           warn
-l7vsd_virtualservice_thread    warn"
+RET=`$L7VSADM -L -x 2>&1 | grep "PARSE ERROR: Argument argc is illegal for -L command."`
+EXPECT="PARSE ERROR: Argument argc is illegal for -L command."
 if [ "${RET}" != "${EXPECT}" ]
 then
-        echo "Test failed: $L7VSADM -V | grep l7vsd_virtualservice"
-        exit 1
-fi
-
-$L7VSADM -L -c vs -l error
-if [ $? -ne 0 ]
-then
-        echo "Test failed: $L7VSADM -L -c vs -l error"
-        exit 1
-fi
-
-RET=`$L7VSADM -V | grep "l7vsd_virtualservice"`
-EXPECT="l7vsd_virtualservice           error
-l7vsd_virtualservice_thread    warn"
-if [ "${RET}" != "${EXPECT}" ]
-then
-        echo "Test failed: $L7VSADM -V | grep l7vsd_virtualservice"
+        echo "Test failed: $L7VSADM -L -x"
         exit 1
 fi
 
